ab8ccc074edde01fb7c2d0e7091163c0ef1d19f9
Emmett Lalish <elalish@gmail.com>
2020-07-18 15:03:21 -0700

    compiles

--- test/mesh_test.cpp
+++ test/mesh_test.cpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <random>
+
 #include "gtest/gtest.h"
 #include "manifold.h"
 #include "meshIO.h"
