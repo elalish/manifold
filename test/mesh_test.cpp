d12c15ceafd815c391a80d9e550c42e339e9bc79
Emmett Lalish <elalish@gmail.com>
2021-03-25 13:14:26 -0700

    corrected expected sponge genus

--- test/mesh_test.cpp
+++ test/mesh_test.cpp
@@ -91,7 +91,7 @@ TEST(Manifold, Regression) {
   Manifold mesh1 = manifold;
   mesh1.Translate(glm::vec3(5.0f));
   int num_overlaps = manifold.NumOverlaps(mesh1);
-  ASSERT_EQ(num_overlaps, 237125);
+  ASSERT_EQ(num_overlaps, 237322);
 
   Mesh mesh_out = manifold.Extract();
   Manifold mesh2(mesh_out);
