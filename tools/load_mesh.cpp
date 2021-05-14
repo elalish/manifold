f190a31b73b3d2d80f453b797471fb93a7e216f2
Emmett Lalish <elalish@gmail.com>
2021-05-13 21:48:19 -0700

    updated copyright dates

--- tools/load_mesh.cpp
+++ tools/load_mesh.cpp
@@ -1,4 +1,4 @@
-// Copyright 2019 Emmett Lalish
+// Copyright 2020 Emmett Lalish
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include <iostream>
+
 #include "manifold.h"
 #include "meshIO.h"
 
