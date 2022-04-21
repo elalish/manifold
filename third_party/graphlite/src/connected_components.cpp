8e17e6c8ba72f87d13fdbd05a31eb1ee6e28c16c
Emmett Lalish <elalish@gmail.com>
2022-04-20 23:24:23 -0700

    added header

--- third_party/graphlite/src/connected_components.cpp
+++ third_party/graphlite/src/connected_components.cpp
@@ -1,3 +1,17 @@
+// Copyright 2022 Emmett Lalish
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <deque>
 
 #include "graph.h"
@@ -12,10 +26,8 @@ int ConnectedComponents(std::vector<int>& components, const Graph& graph) {
   std::fill(components.begin(), components.end(), -1);
 
   std::deque<int> queue;
-  const auto begin = graph.begin();
-  const auto end = graph.end();
   int numComponent = 0;
-  for (auto it = begin; it != end; ++it) {
+  for (auto it = graph.begin(); it != graph.end(); ++it) {
     const int& root = *it;
     if (components[root] >= 0) continue;  // skip visited nodes
 
@@ -28,8 +40,7 @@ int ConnectedComponents(std::vector<int>& components, const Graph& graph) {
       queue.pop_front();
       for (auto n_it = n_begin; n_it != n_end; ++n_it) {
         const int& neighbor = *n_it;
-        if (components[neighbor] < 0) {
-          // traversed node is labeled with the root component label
+        if (components[neighbor] < 0) {  // unvisited
           components[neighbor] = numComponent;
           queue.emplace_back(neighbor);
         }
