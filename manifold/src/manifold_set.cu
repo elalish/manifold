a4db0ae7046600724039e3e07f61e63c1240dd9d
pca006132 <john.lck40@gmail.com>
2022-05-05 21:01:13 +0800

    sort before union to improve performance

--- manifold/src/manifold_set.cu
+++ manifold/src/manifold_set.cu
@@ -2,6 +2,7 @@
 #include <thrust/execution_policy.h>
 #include "manifold_set.h"
 #include "utils.cuh"
+#include "vec_dh.cuh"
 
 namespace manifold {
 
@@ -16,6 +17,12 @@ ManifoldSet::ManifoldSet(const Manifold &manifold)
           std::vector<Manifold>({manifold}))) {}
 
 ManifoldSet ManifoldSet::operator+(const ManifoldSet &other) const {
+  if (manifolds == other.manifolds && transform_ == other.transform_) {
+    return *this;
+  }
+  if (manifolds->empty()) {
+    return other;
+  }
   ManifoldSet result;
   result.manifolds->reserve(manifolds->size() + other.manifolds->size());
   result.manifolds->insert(result.manifolds->end(), manifolds->begin(),
@@ -34,6 +41,14 @@ ManifoldSet ManifoldSet::operator+(const ManifoldSet &other) const {
 }
 
 ManifoldSet &ManifoldSet::operator+=(const ManifoldSet &other) {
+  if (manifolds == other.manifolds && transform_ == other.transform_) {
+      return *this;
+  }
+  if (manifolds->empty()) {
+    manifolds = other.manifolds;
+    transform_ = other.transform_;
+    return *this;
+  }
   if (manifolds.use_count() != 1) {
     auto old = manifolds;
     manifolds = std::make_shared<std::vector<Manifold>>();
@@ -87,12 +102,25 @@ Manifold ManifoldSet::ToManifold() {
   if (manifolds->size() == 0) {
     return Manifold();
   }
+
+  // sort the manifolds by their distance to a point, hopefully
+  // the combined bounding box of the first few manifolds will be
+  // smaller, so we can compose more manifolds together cheaply
+  glm::vec3 min_pt(0);
+  for (auto &m : *manifolds) {
+    Box b = m.BoundingBox();
+    min_pt = glm::min(min_pt, b.min);
+  }
+
+  std::sort(manifolds->begin(), manifolds->end(), 
+          [&min_pt](const Manifold &a, const Manifold &b) {
+            return glm::distance(min_pt, a.BoundingBox().min) < glm::distance(min_pt, b.BoundingBox().min);
+          });
+
   std::vector<std::pair<std::vector<size_t>, Box>> disjointSets;
   for (size_t i = 0; i < manifolds->size(); i++) {
-    manifolds->at(i).GetMesh(); // why?
     Box box = manifolds->at(i).BoundingBox();
-    auto it =
-        std::find_if(disjointSets.begin(), disjointSets.end(),
+    auto it = std::find_if(disjointSets.begin(), disjointSets.end(),
                      [&box](const std::pair<std::vector<size_t>, Box> &p) {
                        return !p.second.DoesOverlap(box);
                      });
@@ -103,11 +131,12 @@ Manifold ManifoldSet::ToManifold() {
       it->second = it->second.Union(box);
     }
   }
-  std::vector<Manifold> results(disjointSets.size());
+
+  std::vector<std::unique_ptr<Manifold>> results(disjointSets.size());
   thrust::for_each_n(thrust::host, countAt(0), disjointSets.size(),
                      [&](size_t i) {
                        if (disjointSets[i].first.size() == 1) {
-                         results[i] = manifolds->at(disjointSets[i].first[0]);
+                         results[i] = std::make_unique<Manifold>(manifolds->at(disjointSets[i].first[0]));
                          return;
                        }
                        std::vector<Manifold> buffer;
@@ -115,19 +144,31 @@ Manifold ManifoldSet::ToManifold() {
                        for (auto &i : disjointSets[i].first) {
                          buffer.push_back(std::move(manifolds->at(i)));
                        }
-                       results[i] = Manifold::Compose(buffer);
+                       results[i] = std::make_unique<Manifold>(Manifold::Compose(buffer));
                      });
-  Manifold result;
-  for (auto &m : results) {
-    result += m;
+
+  auto cmp_fn = [](const std::unique_ptr<Manifold> &a, const std::unique_ptr<Manifold> &b) {
+                  // invert the order because we want a min heap
+                  return a->NumVert() > b->NumVert();
+                };
+
+  // union smaller manifolds first to avoid copying large manifolds repeatedly
+  std::make_heap(results.begin(), results.end(), cmp_fn);
+  while (results.size() > 1) {
+    std::pop_heap(results.begin(), results.end(), cmp_fn);
+    auto a = std::move(results.back());
+    results.pop_back();
+    std::pop_heap(results.begin(), results.end(), cmp_fn);
+    *results.back() += *a;
+    std::push_heap(results.begin(), results.end(), cmp_fn);
   }
 
   manifolds->clear();
-  manifolds->push_back(result);
+  manifolds->push_back(*results.back());
 
   if (transform_ != glm::mat4x3(1.0f)) {
     manifolds = std::make_shared<std::vector<Manifold>>();
-    manifolds->push_back(result);
+    manifolds->push_back(*results.back());
     manifolds->back().Transform(transform_);
     transform_ = glm::mat4x3(1.0f);
   }
