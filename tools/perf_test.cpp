738d0fa05057eb9771d02d92f55cdc0142f6baa3
Emmett Lalish <elalish@google.com>
2022-07-06 12:42:00 -0700

    switch copyright to authors

--- tools/perf_test.cpp
+++ tools/perf_test.cpp
@@ -1,4 +1,4 @@
-// Copyright 2020 Emmett Lalish
+// Copyright 2020 The Manifold Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
