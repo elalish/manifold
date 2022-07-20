200fe2c9603e8d9c96f7bf6532d4bb9a504b8198
Emmett Lalish <elalish@gmail.com>
2022-07-19 21:47:50 -0700

    formatted bindings

--- bindings/wasm/bindings.cpp
+++ bindings/wasm/bindings.cpp
@@ -16,48 +16,42 @@ Manifold Intersection(Manifold& a, Manifold& b) { return a ^ b; }
 void Intersect(Manifold& a, Manifold& b) { a ^= b; }
 
 EMSCRIPTEN_BINDINGS(whatever) {
+  value_object<glm::ivec3>("ivec3")
+      .field("0", &glm::ivec3::x)
+      .field("1", &glm::ivec3::y)
+      .field("2", &glm::ivec3::z);
 
-    value_object<glm::ivec3>("ivec3")
-        .field("0", &glm::ivec3::x)
-        .field("1", &glm::ivec3::y)
-        .field("2", &glm::ivec3::z)
-        ;
+  register_vector<glm::ivec3>("Vector_ivec3");
 
-    register_vector<glm::ivec3>("Vector_ivec3");
+  value_object<glm::vec3>("vec3")
+      .field("x", &glm::vec3::x)
+      .field("y", &glm::vec3::y)
+      .field("z", &glm::vec3::z);
 
-    value_object<glm::vec3>("vec3")
-        .field("x", &glm::vec3::x)
-        .field("y", &glm::vec3::y)
-        .field("z", &glm::vec3::z)
-        ;
+  register_vector<glm::vec3>("Vector_vec3");
 
-    register_vector<glm::vec3>("Vector_vec3");
+  value_object<glm::vec4>("vec4")
+      .field("x", &glm::vec4::x)
+      .field("y", &glm::vec4::y)
+      .field("z", &glm::vec4::z)
+      .field("w", &glm::vec4::w);
 
-    value_object<glm::vec4>("vec4")
-        .field("x", &glm::vec4::x)
-        .field("y", &glm::vec4::y)
-        .field("z", &glm::vec4::z)
-        .field("w", &glm::vec4::w)
-        ;
+  register_vector<glm::vec4>("Vector_vec4");
 
-    register_vector<glm::vec4>("Vector_vec4");
+  value_object<Mesh>("Mesh")
+      .field("vertPos", &Mesh::vertPos)
+      .field("triVerts", &Mesh::triVerts)
+      .field("vertNormal", &Mesh::vertNormal)
+      .field("halfedgeTangent", &Mesh::halfedgeTangent);
 
-    value_object<Mesh>("Mesh")
-        .field("vertPos", &Mesh::vertPos)
-        .field("triVerts", &Mesh::triVerts)
-        .field("vertNormal", &Mesh::vertNormal)
-        .field("halfedgeTangent", &Mesh::halfedgeTangent)
-        ;
+  class_<Manifold>("Manifold")
+      .constructor<Mesh>()
+      .function("Add", &Add)
+      .function("Subtract", &Subtract)
+      .function("Intersect", &Intersect)
+      .function("GetMesh", &Manifold::GetMesh);
 
-    class_<Manifold>("Manifold")
-        .constructor<Mesh>()
-        .function("Add", &Add)
-        .function("Subtract", &Subtract)
-        .function("Intersect", &Intersect)
-        .function("GetMesh", &Manifold::GetMesh)
-        ;
-
-    function("Union", &Union);
-    function("Difference", &Difference);
-    function("Intersection", &Intersection);
+  function("Union", &Union);
+  function("Difference", &Difference);
+  function("Intersection", &Intersection);
 }
