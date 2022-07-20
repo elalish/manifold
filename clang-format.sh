370111b19659427bcec59110b2195f3fc993fca1
Emmett Lalish <elalish@gmail.com>
2022-07-19 21:40:36 -0700

    fix workflows

--- clang-format.sh
+++ clang-format.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env -S bash -e
-for f in {collider,manifold,meshIO,polygon,samples,utilities}/**/*.{h,cpp} {test,tools}/*.cpp; do
+for f in {bindings,samples,extras,src,test}/**/*.{h,cpp}; do
   clang-format --dry-run --Werror $f &
   pids[${i}]=$!
 done
