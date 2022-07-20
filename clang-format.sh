311ca75a21de68e103d03a6d67783e8f29a0dbc6
Emmett Lalish <elalish@gmail.com>
2022-07-19 22:31:08 -0700

    actions format check

--- clang-format.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env -S bash -e
-for f in {bindings,samples,extras,src,test}/**/*.{h,cpp}; do
-  clang-format --dry-run --Werror $f &
-  pids[${i}]=$!
-done
-
-for pid in ${pids[*]}; do
-    wait $pid
-done
