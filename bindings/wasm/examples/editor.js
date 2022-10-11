9c6bd4939e32f2cdb6f31ca1446e2b535195888e
Emmett Lalish <elalish@gmail.com>
2022-10-11 09:47:51 -0700

    moved computations to worker

--- bindings/wasm/examples/editor.js
+++ bindings/wasm/examples/editor.js
@@ -1,4 +1,5 @@
 let editor = undefined;
+let manifoldWorker = new Worker('worker.js');
 
 // File UI ------------------------------------------------------------
 const fileButton = document.querySelector('#file');
@@ -168,8 +169,13 @@ function newItem(code) {
 const newButton = document.querySelector('#new');
 newButton.onclick = function () { newItem(''); };
 
+const runButton = document.querySelector('#compile');
+const downloadButton = document.querySelector('#download');
+const consoleElement = document.querySelector('#console');
+let manifoldInitialized = false;
+
 // Editor ------------------------------------------------------------
-let worker = undefined;
+let tsWorker = undefined;
 require.config({ paths: { vs: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.34.0/min/vs' } });
 require(['vs/editor/editor.main'], async function () {
   const content = await fetch('bindings.d.ts').then(response => response.text());
@@ -179,7 +185,7 @@ require(['vs/editor/editor.main'], async function () {
     automaticLayout: true
   });
   const w = await monaco.languages.typescript.getTypeScriptWorker();
-  worker = await w(editor.getModel().uri);
+  tsWorker = await w(editor.getModel().uri);
 
   for (const [name] of examples) {
     appendDropdownItem(name);
@@ -201,7 +207,10 @@ require(['vs/editor/editor.main'], async function () {
   }
   switchTo(currentName);
 
-  if (safe2Execute) { document.querySelector('#compile').click(); }
+  if (safe2Execute && manifoldInitialized) {
+    runButton.disabled = false;
+    runButton.click();
+  }
 
   editor.onDidChangeModelContent(e => {
     runButton.disabled = false;
@@ -222,8 +231,7 @@ require(['vs/editor/editor.main'], async function () {
 });
 
 // Execution ------------------------------------------------------------
-const runButton = document.querySelector('#compile');
-const consoleElement = document.querySelector('#console');
+
 
 const oldLog = console.log;
 console.log = function (message) {
@@ -236,164 +244,44 @@ function clearConsole() {
   consoleElement.textContent = '';
 }
 
-var Module = {
-  onRuntimeInitialized: function () {
-    Module.setup();
-    // Setup memory management, such that users don't have to care about
-    // calling `delete` manually.
-    // Note that this only fixes memory leak across different runs: the memory
-    // will only be freed when the compilation finishes.
+let t0 = performance.now();
 
-    // manifold member functions that returns a new manifold
-    const memberFunctions = [
-      'add', 'subtract', 'intersect', 'refine', 'transform', 'translate', 'rotate',
-      'scale', 'asOriginal', 'smooth', 'decompose'
-    ];
-    // top level functions that constructs a new manifold
-    const constructors = [
-      'cube', 'cylinder', 'sphere', 'tetrahedron', 'extrude', 'revolve', 'union',
-      'difference', 'intersection', 'compose', 'levelSet'
-    ];
-    const utils = [
-      'setMinCircularAngle', 'setMinCircularEdgeLength', 'setCircularSegments',
-      'getCircularSegments'
-    ];
-    const exposedFunctions = constructors.concat(utils);
-
-    let manifoldRegistry = [];
-    for (const name of memberFunctions) {
-      const originalFn = Module.Manifold.prototype[name];
-      Module.Manifold.prototype["_" + name] = originalFn;
-      Module.Manifold.prototype[name] = function (...args) {
-        const result = this["_" + name](...args);
-        manifoldRegistry.push(result);
-        return result;
-      }
-    }
-
-    for (const name of constructors) {
-      const originalFn = Module[name];
-      Module[name] = function (...args) {
-        const result = originalFn(...args);
-        manifoldRegistry.push(result);
-        return result;
-      }
-    }
-
-    Module.cleanup = function () {
-      for (const obj of manifoldRegistry) {
-        // decompose result is an array of manifolds
-        if (obj instanceof Array)
-          for (const elem of obj)
-            elem.delete();
-        else
-          obj.delete();
-      }
-      manifoldRegistry = [];
-    }
-
-    runButton.onclick = async function (e) {
-      saveCurrent();
-      setScript('safe', 'false');
-      runButton.disabled = true;
-      clearConsole();
-      console.log('Running...');
-      const output = await worker.getEmitOutput(editor.getModel().uri.toString());
-      const content = output.outputFiles[0].text + 'push2MV(result);';
-      try {
-        const f = new Function(...exposedFunctions, content);
-        const t0 = performance.now();
-        f(...exposedFunctions.map(name => Module[name]));
-        const t1 = performance.now();
-        const log = consoleElement.textContent;
-        // Remove "Running..."
-        consoleElement.textContent = log.substring(log.indexOf("\n") + 1);
-        console.log(`Took ${Math.round(t1 - t0)} ms`);
-        setScript('safe', 'true');
-      } catch (error) {
-        console.log(error);
-      } finally {
-        Module.cleanup();
-      }
-    };
-  }
+runButton.onclick = async function () {
+  saveCurrent();
+  setScript('safe', 'false');
+  runButton.disabled = true;
+  clearConsole();
+  console.log('Running...');
+  const output = await tsWorker.getEmitOutput(editor.getModel().uri.toString());
+  manifoldWorker.postMessage(output.outputFiles[0].text);
+  t0 = performance.now();
 };
 
-// Export & Rendering ------------------------------------------------------------
 const mv = document.querySelector('model-viewer');
-const mesh = new THREE.Mesh(undefined, new THREE.MeshStandardMaterial({
-  color: 'yellow',
-  metalness: 1,
-  roughness: 0.2
-}));
-const rotation = new THREE.Matrix4();
-rotation.set(
-  1, 0, 0, 0,
-  0, 0, 1, 0,
-  0, -1, 0, 0,
-  0, 0, 0, 1);
-mesh.setRotationFromMatrix(rotation); // Z-up -> Y-up
-mesh.scale.setScalar(0.001); // mm -> m
-
 let objectURL = null;
-const exporter = new THREE.GLTFExporter();
 
-function push2MV(manifold) {
-  const box = manifold.boundingBox();
-  const size = [0, 0, 0];
-  for (let i = 0; i < 3; i++) {
-    size[i] = Math.round((box.max[i] - box.min[i]) * 10) / 10;
+manifoldWorker.onmessage = function (e) {
+  if (e.data == null && !manifoldInitialized) {
+    manifoldInitialized = true;
+    if (tsWorker != null) {
+      runButton.disabled = false;
+      runButton.click();
+    }
   }
-  console.log(`Bounding Box: X = ${size[0]} mm, Y = ${size[1]} mm, Z = ${size[2]} mm`);
-  mesh.geometry?.dispose();
-  mesh.geometry = mesh2geometry(manifold.getMesh());
-  exporter.parse(
-    mesh,
-    (gltf) => {
-      const blob = new Blob([gltf], { type: 'application/octet-stream' });
-      URL.revokeObjectURL(objectURL);
-      objectURL = URL.createObjectURL(blob);
-      mv.src = objectURL;
-    },
-    () => console.log('glTF export failed!'),
-    { binary: true }
-  );
+
+  const t1 = performance.now();
+  const log = consoleElement.textContent;
+  // Remove "Running..."
+  consoleElement.textContent = log.substring(log.indexOf("\n") + 1);
+  console.log(`Took ${Math.round(t1 - t0)} ms`);
+  setScript('safe', 'true');
+
+  URL.revokeObjectURL(objectURL);
+  objectURL = e.data;
+  mv.src = objectURL;
 }
 
-function mesh2geometry(mesh) {
-  const geometry = new THREE.BufferGeometry();
-
-  const numVert = mesh.vertPos.size();
-  const vert = new Float32Array(3 * numVert);
-  for (let i = 0; i < numVert; i++) {
-    const v = mesh.vertPos.get(i);
-    const idx = 3 * i;
-    vert[idx] = v.x;
-    vert[idx + 1] = v.y;
-    vert[idx + 2] = v.z;
-  }
-
-  const numTri = mesh.triVerts.size();
-  const tri = new Uint32Array(3 * numTri);
-  for (let i = 0; i < numTri; i++) {
-    const v = mesh.triVerts.get(i);
-    const idx = 3 * i;
-    tri[idx] = v[0];
-    tri[idx + 1] = v[1];
-    tri[idx + 2] = v[2];
-  }
-
-  mesh.vertPos.delete();
-  mesh.triVerts.delete();
-  mesh.vertNormal.delete();
-  mesh.halfedgeTangent.delete();
-
-  geometry.setAttribute('position', new THREE.BufferAttribute(vert, 3));
-  geometry.setIndex(new THREE.BufferAttribute(tri, 1));
-  return geometry;
-}
-
-document.querySelector('#download').onclick = function () {
+downloadButton.onclick = function () {
   const link = document.createElement("a");
   link.download = "manifold.glb";
   link.href = objectURL;
