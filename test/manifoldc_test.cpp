a550b8eebf676c80fffa006127ef52a739f37951
Geoff deRosenroll <geoffderosenroll@gmail.com>
2022-12-17 15:58:05 -0800

    Add tests from removed testc

--- test/manifoldc_test.cpp
+++ test/manifoldc_test.cpp
@@ -31,6 +31,7 @@ TEST(CBIND, sphere) {
 
   EXPECT_EQ(manifold_status(sphere), NO_ERROR);
   EXPECT_EQ(manifold_num_tri(sphere), n * n * 8);
+
   manifold_delete_manifold(sphere);
 }
 
@@ -64,4 +65,66 @@ TEST(CBIND, warp_translation) {
   manifold_delete_mesh(warped_mesh);
   delete trans_verts;
   delete warped_verts;
+}
+
+TEST(CBIND, level_set) {
+  size_t sz = manifold_manifold_size();
+  // can't convert lambda with captures to funptr
+  float (*sdf)(float, float, float) = [](float x, float y, float z) {
+    const float radius = 15;
+    const float xscale = 3;
+    const float yscale = 1;
+    const float zscale = 1;
+    float xs = x / xscale;
+    float ys = y / yscale;
+    float zs = z / zscale;
+    return radius - sqrt(xs * xs + ys * ys + zs * zs);
+  };
+
+  const float bb = 30;  // (radius * 2)
+  // bounding box scaled according to factors used in *sdf
+  ManifoldBox *bounds = manifold_box(malloc(manifold_box_size()), -bb * 3,
+                                     -bb * 1, -bb * 1, bb * 3, bb * 1, bb * 1);
+  ManifoldMesh *sdf_mesh =
+      manifold_level_set(malloc(manifold_mesh_size()), sdf, bounds, 0.5, 0);
+  ManifoldManifold *sdf_man = manifold_of_mesh(malloc(sz), sdf_mesh);
+
+#ifdef MANIFOLD_EXPORT
+  ManifoldExportOptions *options =
+      manifold_export_options(malloc(manifold_export_options_size()));
+  const char *name = "cbind_sdf_test.stl";
+  manifold_export_mesh(name, sdf_mesh, options);
+  manifold_delete_export_options(options);
+#endif
+
+  EXPECT_EQ(manifold_status(sdf_man), NO_ERROR);
+
+  manifold_delete_mesh(sdf_mesh);
+  manifold_delete_manifold(sdf_man);
+  manifold_delete_box(bounds);
+}
+
+TEST(CBIND, extrude) {
+  size_t sz = manifold_manifold_size();
+
+  ManifoldVec2 pts[] = {{0, 0}, {1, 0}, {1, 1}, {0, 1}};
+  ManifoldSimplePolygon *sq[] = {manifold_simple_polygon(
+      malloc(manifold_simple_polygon_size()), &pts[0], 4)};
+  ManifoldPolygons *polys =
+      manifold_polygons(malloc(manifold_polygons_size()), sq, 1);
+
+  ManifoldManifold *cube = manifold_cube(malloc(sz), 1., 1., 1., 0);
+  ManifoldManifold *extrusion =
+      manifold_extrude(malloc(sz), polys, 1, 0, 0, 1, 1);
+
+  ManifoldManifold *diff = manifold_difference(malloc(sz), cube, extrusion);
+  ManifoldProperties props = manifold_get_properties(diff);
+
+  EXPECT_TRUE(props.volume < eps);
+
+  manifold_delete_manifold(cube);
+  manifold_delete_manifold(extrusion);
+  manifold_delete_manifold(diff);
+  manifold_delete_simple_polygon(sq[0]);
+  manifold_delete_polygons(polys);
 }
