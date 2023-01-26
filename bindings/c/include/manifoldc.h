05f9ceb99938ad5235ce6f02a67a8c287cf4f51d
Geoff deRosenroll <geoffderosenroll@gmail.com>
2023-01-26 08:03:29 -0800

    Remove manifold_meshgl_vert_normal from header (impl gone) (#310)
    Remove meshgl_vert_normal from header (impl gone)

--- bindings/c/include/manifoldc.h
+++ bindings/c/include/manifoldc.h
@@ -186,7 +186,6 @@ size_t manifold_meshgl_merge_length(ManifoldMeshGL *m);
 size_t manifold_meshgl_tangent_length(ManifoldMeshGL *m);
 float *manifold_meshgl_vert_properties(void *mem, ManifoldMeshGL *m);
 uint32_t *manifold_meshgl_tri_verts(void *mem, ManifoldMeshGL *m);
-float *manifold_meshgl_vert_normal(void *mem, ManifoldMeshGL *m);
 uint32_t *manifold_meshgl_merge_from_vert(void *mem, ManifoldMeshGL *m);
 uint32_t *manifold_meshgl_merge_to_vert(void *mem, ManifoldMeshGL *m);
 float *manifold_meshgl_halfedge_tangent(void *mem, ManifoldMeshGL *m);
