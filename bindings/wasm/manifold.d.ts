0449a11c351ec6807effed39892cccecbb214265
rafern <rafern@protonmail.com>
2023-03-01 11:40:57 +0000

    Copy type definitions to editor folder (not used for now)

--- bindings/wasm/manifold.d.ts
+++ bindings/wasm/manifold.d.ts
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import * as T from './encapsulated-types.js';
-export * from './global-types.js';
+import * as T from './manifold-encapsulated-types';
+export * from './manifold-global-types';
 
 export type Manifold = T.Manifold;
 export type Mesh = T.Mesh;
