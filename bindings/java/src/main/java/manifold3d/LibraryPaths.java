d26f13b84cead932baac41885cd088c2bdd84128
John Collins <collins@braincorporation.com>
2023-06-08 01:59:27 -0700

    Add LibraryPaths file

---
+++ bindings/java/src/main/java/manifold3d/LibraryPaths.java
@@ -0,0 +1,7 @@
+package manifold3d;
+
+public class LibraryPaths {
+    public static final String MANIFOLD_BUILD_DIR = "../../build";
+    public static final String MANIFOLD_LIB_DIR = MANIFOLD_BUILD_DIR + "/src/manifold";
+    public static final String MESHIO_LIB_DIR = MANIFOLD_BUILD_DIR + "/meshIO";
+}
