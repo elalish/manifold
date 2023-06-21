dceafba6faeedd75ff8791e00d66bf964712d7fb
Johnny Bigert <johnny@getencube.com>
2023-06-21 14:31:40 +0200

    Removed duplicate point in polygon in Python test case

--- bindings/python/examples/missing_polygon.py
+++ bindings/python/examples/missing_polygon.py
@@ -12,7 +12,7 @@ def run():
                 (-175.96430934559382, 60.26716470718384), (-175.38696566496216, 60.26717407055541),
                 (-175.386962543836, 59.26716470718384), (-174.80960083007815, 59.26714979522481),
                 (-174.8096004832914, 58.26716470718384), (-174.23225470415798, 58.26715427684856),
-                (-175.01954376826416, 59.63078409350792), (-176.5416564941429, 62.267156291299955),
+                (-175.01954376826416, 59.63078409350792),
             ]]
         )
     test_cross = \
@@ -28,7 +28,7 @@ def run():
     diff = test_cross - cross_section  # we expect full overlap, so diff should be zero
     if diff.area() > 0.01:
         print(f"Cross-section is missing a part of the polygon! "
-              f"Area: {diff.area()}, "
+              f"Area should be empty: {diff.area()}, "
               f"to_polygons={cross_section.to_polygons()}")
 
     manifold = cross_section.extrude(1.0)
