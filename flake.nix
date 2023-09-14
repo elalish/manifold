206871ac11f485e38e6e489815ba724b8d5ed911
Chun Kit LAM <john.lck40@gmail.com>
2023-09-15 00:42:12 +0800

    dependency update (#559)
    * dependency update
    
    * revert BUILD_SHARED_LIBS changes
    
    * test
    
    * .
    
    * don't try weird stuff with windows
    
    * update
    
    * update CI

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
     url = "github:google/googletest/v1.14.0";
     flake = false;
   };
-
   outputs = { self, nixpkgs, flake-utils, gtest-src }:
     flake-utils.lib.eachDefaultSystem
       (system:
@@ -14,6 +13,37 @@
             inherit system;
             config.allowUnfree = true;
           };
+          thrust-210 = pkgs.nvidia-thrust.overrideAttrs (_: _: {
+            version = "24486a1";
+            src = builtins.fetchGit {
+              url = "https://github.com/NVIDIA/thrust.git";
+              ref = "main";
+              rev = "24486a169a62a58ef8f824d3dc9613c006b6f5a7";
+              submodules = true;
+            };
+            cmakeFlags = [ "-DTHRUST_ENABLE_HEADER_TESTING=OFF" "-DTHRUST_ENABLE_TESTING=OFF" "-DTHRUST_ENABLE_EXAMPLES=OFF" "-DTHRUST_DEVICE_SYSTEM=CPP" ];
+            fixupPhase = ''
+              cat <<EOT > $out/lib/cmake/thrust/thrust-header-search.cmake
+              # Parse version information from version.h in source tree
+              set(_THRUST_VERSION_INCLUDE_DIR "$out/include")
+              if(EXISTS "\''${_THRUST_VERSION_INCLUDE_DIR}/thrust/version.h")
+                set(_THRUST_VERSION_INCLUDE_DIR "\''${_THRUST_VERSION_INCLUDE_DIR}" CACHE FILEPATH "" FORCE) # Clear old result
+                set_property(CACHE _THRUST_VERSION_INCLUDE_DIR PROPERTY TYPE INTERNAL)
+              endif()
+              EOT
+              cat <<EOT > $out/lib/cmake/libcudacxx/libcudacxx-header-search.cmake
+              # Parse version information from version header:
+              unset(_libcudacxx_VERSION_INCLUDE_DIR CACHE) # Clear old result to force search
+              find_path(_libcudacxx_VERSION_INCLUDE_DIR cuda/std/detail/__config
+                NO_DEFAULT_PATH # Only search explicit paths below:
+                PATHS
+                  "\''${CMAKE_CURRENT_LIST_DIR}/../../../include" # Source tree
+              )
+              set_property(CACHE _libcudacxx_VERSION_INCLUDE_DIR PROPERTY TYPE INTERNAL)
+              EOT
+            '';
+            enableParallelBuilding = false;
+          });
           manifold =
             { parallel-backend ? "none"
             , doCheck ? true
@@ -26,6 +56,8 @@
               src = self;
               nativeBuildInputs = (with pkgs; [
                 cmake
+                thrust-210
+                glm
                 ninja
                 (python39.withPackages
                   (ps: with ps; [ trimesh ]))
@@ -49,20 +81,15 @@
             { parallel-backend = "none"; }
             {
               parallel-backend = "tbb";
-              build-tools = with pkgs; [ tbb pkg-config ];
+              build-tools = with pkgs; [ tbb_2021_8 pkg-config ];
             }
           ];
           devShell = { additional ? [ ] }: pkgs.mkShell {
             buildInputs = with pkgs; [
               cmake
-              llvmPackages_13.openmp
-              clang-tools
-              clang_13
-              emscripten
-              tbb
-              lcov
+              tbb_2021_8
+              thrust-210
               gtest
-              tracy
             ] ++ additional;
           };
         in
@@ -85,7 +112,10 @@
                 export EM_CACHE=$(pwd)/.emscriptencache
                 mkdir build
                 cd build
-                emcmake cmake -DCMAKE_BUILD_TYPE=Release -DFETCHCONTENT_SOURCE_DIR_GOOGLETEST=${gtest-src} ..
+                emcmake cmake -DCMAKE_BUILD_TYPE=Release \
+                -DFETCHCONTENT_SOURCE_DIR_GLM=${pkgs.glm.src} \
+                -DFETCHCONTENT_SOURCE_DIR_GOOGLETEST=${gtest-src} \
+                -DFETCHCONTENT_SOURCE_DIR_THRUST=${thrust-210.src} ..
               '';
               buildPhase = ''
                 emmake make -j''${NIX_BUILD_CORES}
@@ -110,7 +140,9 @@
                 numpy
               ];
               buildInputs = with pkgs; [
-                tbb
+                tbb_2021_8
+                thrust-210
+                glm
               ];
               nativeBuildInputs = with pkgs; [
                 cmake
