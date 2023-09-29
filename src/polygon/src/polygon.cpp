1bff99adc0291b7995eaec0be612bde33b1ed575
Emmett Lalish <elalish@google.com>
2023-09-29 15:49:44 -0700

    another test, another fix

--- src/polygon/src/polygon.cpp
+++ src/polygon/src/polygon.cpp
@@ -636,8 +636,8 @@ class EarClip {
       const glm::vec2 diff = vert->pos - start->pos;
       const float d2 = glm::dot(diff, diff);
       if (d2 < minD2 && vert->pos.y * above > start->pos.y * above &&
-          above * glm::determinant(glm::mat2(left, offset)) > 0 &&
-          above * glm::determinant(glm::mat2(offset, right)) > 0 &&
+          above * glm::determinant(glm::mat2(left, offset)) > -precision_ &&
+          above * glm::determinant(glm::mat2(offset, right)) > -precision_ &&
           !vert->IsConvex(precision_)) {
         minD2 = d2;
         best = vert;
