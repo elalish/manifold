fafe4792da71e4bfc95550baafa1ede1c927f3c2
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2023-11-12 17:30:26 -0500

    Prepare a test input with failing boolean (#609)
    * Prepare a test input with failing boolean
    
    Encountered this case when testing Manifold with the BRL-CAD example
    file - specifically, the r.glass8 region.  C++ exception "Vec out of
    range" is thrown.  Both inputs are the outputs of previous Manifold
    boolean evaluations.
    
    * disabled Havocglass8Bool test
    
    ---------
    
    Co-authored-by: pca006132 <john.lck40@gmail.com>

--- test/samples_test.cpp
+++ test/samples_test.cpp
@@ -268,6 +268,19 @@ TEST(Samples, SelfIntersect) {
 }
 #endif
 
+#ifdef MANIFOLD_EXPORT
+TEST(Samples, DISABLED_Havocglass8Bool) {
+  manifold::PolygonParams().processOverlaps = true;
+  std::string file = __FILE__;
+  std::string dir = file.substr(0, file.rfind('/'));
+  Manifold m1 = ImportMesh(dir + "/models/Havocglass8_left.glb");
+  Manifold m2 = ImportMesh(dir + "/models/Havocglass8_right.glb");
+  Manifold res = m1 + m2;  // Union
+  res.GetMeshGL();         // test crash
+  manifold::PolygonParams().processOverlaps = false;
+}
+#endif
+
 TEST(Samples, CondensedMatter16) {
   // FIXME: it should be geometrically valid
   manifold::PolygonParams().processOverlaps = true;
