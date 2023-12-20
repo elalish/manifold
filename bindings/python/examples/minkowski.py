5f52540406751f738c5abe768acb87d8e50563a6
Johnathon Selstad <makeshifted@gmail.com>
2023-12-19 23:42:59 -0800

    Make Python Test Cheaper so it doesn't segfault

--- bindings/python/examples/minkowski.py
+++ bindings/python/examples/minkowski.py
@@ -16,7 +16,7 @@ def run():
     ]:
         star += Manifold.hull_points(np.concatenate((cube_vertices, offset), axis=0))
 
-    sphere = Manifold.sphere(0.6, 30)
+    sphere = Manifold.sphere(0.6, 20)
     cube = Manifold.cube([1.0, 1.0, 1.0], True)
     sphereless_cube = cube - sphere
     return Manifold.minkowski(sphereless_cube, star, False)
