d51976a9da11fc83d5f8b95880bb50fffc9669e1
Johnathon Selstad <makeshifted@gmail.com>
2023-12-19 23:31:05 -0800

    Fix Python Test

--- bindings/python/examples/minkowski.py
+++ bindings/python/examples/minkowski.py
@@ -3,7 +3,8 @@ from manifold3d import Manifold
 
 
 def run():
-    small_cube = Manifold.cube(0.1, 0.1, 0.1, True)
+    small_cube = Manifold.cube([0.1, 0.1, 0.1], True)
+    cube_vertices = small_cube.to_mesh().vert_properties[:, :3]
     star = Manifold.as_original(small_cube)
     for offset in [
         [[0.2, 0.0, 0.0]],
@@ -13,11 +14,7 @@ def run():
         [[0.0, 0.0, 0.2]],
         [[0.0, 0.0, -0.2]],
     ]:
-        star += Manifold.hull_points(
-            np.concatenate(
-                (small_cube.to_mesh().vert_properties[:, :3], offset), axis=0
-            )
-        )
+        star += Manifold.hull_points(np.concatenate((cube_vertices, offset), axis=0))
 
     sphere = Manifold.sphere(0.6, 30)
     cube = Manifold.cube(1.0, 1.0, 1.0, True)
