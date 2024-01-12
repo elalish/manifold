e5992088a7f0d4cc9e759e31eb2e3b563f3bbe92
pca006132 <john.lck40@gmail.com>
2024-01-12 21:27:39 +0800

    cmake packaging

--- flake.nix
+++ flake.nix
@@ -5,7 +5,15 @@
     url = "github:google/googletest/v1.14.0";
     flake = false;
   };
-  outputs = { self, nixpkgs, flake-utils, gtest-src }:
+  inputs.thrust-src = {
+    url = "git+https://github.com/NVIDIA/thrust.git?submodules=1";
+    flake = false;
+  };
+  inputs.clipper2-src = {
+    url = "github:AngusJohnson/Clipper2/Clipper2_1.3.0";
+    flake = false;
+  };
+  outputs = { self, nixpkgs, flake-utils, gtest-src, thrust-src, clipper2-src }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
@@ -13,36 +21,9 @@
             inherit system;
             config.allowUnfree = true;
           };
-          thrust-210 = pkgs.nvidia-thrust.overrideAttrs (_: _: {
-            version = "24486a1";
-            src = builtins.fetchGit {
-              url = "https://github.com/NVIDIA/thrust.git";
-              ref = "main";
-              rev = "24486a169a62a58ef8f824d3dc9613c006b6f5a7";
-              submodules = true;
-            };
-            cmakeFlags = [ "-DTHRUST_ENABLE_HEADER_TESTING=OFF" "-DTHRUST_ENABLE_TESTING=OFF" "-DTHRUST_ENABLE_EXAMPLES=OFF" "-DTHRUST_DEVICE_SYSTEM=CPP" ];
-            fixupPhase = ''
-              cat <<EOT > $out/lib/cmake/thrust/thrust-header-search.cmake
-              # Parse version information from version.h in source tree
-              set(_THRUST_VERSION_INCLUDE_DIR "$out/include")
-              if(EXISTS "\''${_THRUST_VERSION_INCLUDE_DIR}/thrust/version.h")
-                set(_THRUST_VERSION_INCLUDE_DIR "\''${_THRUST_VERSION_INCLUDE_DIR}" CACHE FILEPATH "" FORCE) # Clear old result
-                set_property(CACHE _THRUST_VERSION_INCLUDE_DIR PROPERTY TYPE INTERNAL)
-              endif()
-              EOT
-              cat <<EOT > $out/lib/cmake/libcudacxx/libcudacxx-header-search.cmake
-              # Parse version information from version header:
-              unset(_libcudacxx_VERSION_INCLUDE_DIR CACHE) # Clear old result to force search
-              find_path(_libcudacxx_VERSION_INCLUDE_DIR cuda/std/detail/__config
-                NO_DEFAULT_PATH # Only search explicit paths below:
-                PATHS
-                  "\''${CMAKE_CURRENT_LIST_DIR}/../../../include" # Source tree
-              )
-              set_property(CACHE _libcudacxx_VERSION_INCLUDE_DIR PROPERTY TYPE INTERNAL)
-              EOT
-            '';
-            enableParallelBuilding = false;
+          clipper2 = pkgs.clipper2.overrideAttrs (_: _: {
+            version = "1.3.0";
+            src = clipper2-src;
           });
           manifold =
             { parallel-backend ? "none"
@@ -56,17 +37,21 @@
               src = self;
               nativeBuildInputs = (with pkgs; [
                 cmake
-                thrust-210
-                glm
                 ninja
-                (python39.withPackages
+                (python3.withPackages
                   (ps: with ps; [ trimesh pytest ]))
                 gtest
               ]) ++ build-tools;
+              buildInputs = with pkgs; [
+                glm
+                clipper2
+                assimp
+              ];
               cmakeFlags = [
                 "-DMANIFOLD_PYBIND=ON"
                 "-DMANIFOLD_CBIND=ON"
                 "-DBUILD_SHARED_LIBS=ON"
+                "-DFETCHCONTENT_SOURCE_DIR_THRUST=${thrust-src}"
                 "-DMANIFOLD_PAR=${pkgs.lib.strings.toUpper parallel-backend}"
               ];
               checkPhase = ''
@@ -85,16 +70,8 @@
               build-tools = with pkgs; [ tbb_2021_8 pkg-config ];
             }
           ];
-          devShell = { additional ? [ ] }: pkgs.mkShell {
-            buildInputs = with pkgs; [
-              cmake
-              tbb_2021_8
-              thrust-210
-              gtest
-            ] ++ additional;
-          };
         in
-        {
+        rec {
           packages = (builtins.listToAttrs
             (map
               (x: {
@@ -116,7 +93,7 @@
                 emcmake cmake -DCMAKE_BUILD_TYPE=Release \
                 -DFETCHCONTENT_SOURCE_DIR_GLM=${pkgs.glm.src} \
                 -DFETCHCONTENT_SOURCE_DIR_GOOGLETEST=${gtest-src} \
-                -DFETCHCONTENT_SOURCE_DIR_THRUST=${thrust-210.src} ..
+                -DFETCHCONTENT_SOURCE_DIR_THRUST=${thrust-src} ..
               '';
               buildPhase = ''
                 emmake make -j''${NIX_BUILD_CORES}
@@ -142,7 +119,6 @@
               ];
               buildInputs = with pkgs; [
                 tbb_2021_8
-                thrust-210
                 glm
               ];
               nativeBuildInputs = with pkgs; [
@@ -153,6 +129,9 @@
                 pyproject-metadata
                 pathspec
                 pkg-config
+              ];
+              cmakeFlags = [
+                "-DFETCHCONTENT_SOURCE_DIR_THRUST=${thrust-src}"
               ];
               checkInputs = [
                 trimesh
@@ -167,7 +146,13 @@
               '';
             };
           };
-          devShell = devShell { };
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              cmake
+              tbb_2021_8
+              gtest
+            ];
+          };
         }
       );
 }
