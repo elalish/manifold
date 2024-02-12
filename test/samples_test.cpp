1279204930c34e2161198e8cb69532d7b829674e
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2024-02-12 18:38:34 -0500

    Prepare a test case for "SparseIndices too large" core dump

--- test/samples_test.cpp
+++ test/samples_test.cpp
@@ -317,6 +317,17 @@ TEST(Samples, GenericTwinBooleanTest7863) {
   manifold::PolygonParams().processOverlaps = false;
 }
 
+TEST(Samples, GenericTwinBooleanTest7081) {
+  manifold::PolygonParams().processOverlaps = true;
+  std::string file = __FILE__;
+  std::string dir = file.substr(0, file.rfind('/'));
+  Manifold m1 = ImportMesh(dir + "/models/Generic_Twin_7081.1.t0_left.glb");
+  Manifold m2 = ImportMesh(dir + "/models/Generic_Twin_7081.1.t0_right.glb");
+  Manifold res = m1 + m2;  // Union
+  res.GetMeshGL();         // test crash
+  manifold::PolygonParams().processOverlaps = false;
+}
+
 TEST(Samples, Havocglass8Bool) {
   manifold::PolygonParams().processOverlaps = true;
   std::string file = __FILE__;
