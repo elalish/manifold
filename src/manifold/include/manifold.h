3f532aaac38685e7f1cf9ae9f9e16ddcac51dff8
Kushal-Shah-03 <kushal.s@students.iiit.ac.in>
2024-03-29 03:47:21 +0530

    Attempt to fix formatting

--- src/manifold/include/manifold.h
+++ src/manifold/include/manifold.h
@@ -244,7 +244,7 @@ class Manifold {
   Manifold SetProperties(
       int, std::function<void(float*, glm::vec3, const float*)>) const;
   Manifold CalculateCurvature(int gaussianIdx, int meanIdx) const;
-    Manifold CalculateNormals(int normalIdx, float minSharpAngle = 60) const;
+  Manifold CalculateNormals(int normalIdx, float minSharpAngle = 60) const;
   Manifold SmoothByNormals(int normalIdx) const;
   Manifold SmoothOut(float minSharpAngle = 60, float minSmoothness = 0) const;
   Manifold Refine(int) const;
