e396695f0d7f685761a70e4109b322f7d2546fbd
Peter Harman <peter.harman@deltatheta.com>
2024-04-26 10:32:18 +0100

    Revert "Correct more formatting"
    This reverts commit 6e409cf0a9e2c000bddd70ba41667d3209410d4a.

--- test/manifold_test.cpp
+++ test/manifold_test.cpp
@@ -161,7 +161,8 @@ TEST(Manifold, Decompose) {
 }
 
 TEST(Manifold, DecomposeProps) {
-  auto posNorm = [](float* newProp, glm::vec3 pos, const float* oldProp) {
+  auto posNorm = [](float* newProp, glm::vec3 pos,
+                           const float* oldProp) {
     newProp[0] = hypotf(hypotf(pos.x, pos.y), pos.z);
   };
   std::vector<Manifold> manifoldList;
