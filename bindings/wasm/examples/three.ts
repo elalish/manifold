c01f5df49a9fd5a766cfde55cba5412dc7d77f5e
Emmett Lalish <elalish@google.com>
2024-05-09 22:24:05 -0700

    added docs

--- bindings/wasm/examples/three.ts
+++ bindings/wasm/examples/three.ts
@@ -16,61 +16,36 @@ import {BoxGeometry, BufferAttribute, BufferGeometry, IcosahedronGeometry, Mesh 
 
 import Module, {Mesh} from './built/manifold.js';
 
-type BooleanOp = 'union'|'difference'|'intersection';
-
+// Load Manifold WASM library
 const wasm = await Module();
 wasm.setup();
-
 const {Manifold, Mesh} = wasm;
 
-// we have manifold module, let's do some three.js
-const camera = new PerspectiveCamera(30, 1, 0.01, 10);
-camera.position.z = 1;
-
-const scene = new Scene();
-scene.add(camera);
-camera.add(new PointLight(0xffffff, 1));
-
+// Define our set of materials
 const materials = [
   new MeshNormalMaterial({flatShading: true}),
   new MeshLambertMaterial({color: 'red', flatShading: true}),
   new MeshLambertMaterial({color: 'blue', flatShading: true})
 ];
+const result = new ThreeMesh(undefined, materials);
+
+// Set up Manifold IDs corresponding to materials
 const firstID = Manifold.reserveIDs(materials.length);
+// ids vector is parallel to materials vector - same indexing
 const ids = [...Array<number>(materials.length)].map((_, idx) => firstID + idx);
+// Build a mapping to get back from ID to material index
 const id2matIndex = new Map();
 ids.forEach((id, idx) => id2matIndex.set(id, idx));
 
-const result = new ThreeMesh(undefined, materials);
+// Set up Three.js scene
+const scene = new Scene();
+const camera = new PerspectiveCamera(30, 1, 0.01, 10);
+camera.position.z = 1;
+camera.add(new PointLight(0xffffff, 1));
+scene.add(camera);
 scene.add(result);
 
-const cube = new BoxGeometry(0.2, 0.2, 0.2);
-cube.clearGroups();
-cube.addGroup(0, 18, 0);
-cube.addGroup(18, Infinity, 1);
-
-const icosahedron = new IcosahedronGeometry(0.16);
-icosahedron.clearGroups();
-icosahedron.addGroup(0, 30, 0);
-icosahedron.addGroup(30, Infinity, 2);
-
-const manifold_1 = new Manifold(geometry2mesh(cube));
-const manifold_2 = new Manifold(geometry2mesh(icosahedron));
-
-const csg = function(operation: BooleanOp) {
-  result.geometry?.dispose();
-  result.geometry =
-      mesh2geometry(Manifold[operation](manifold_1, manifold_2).getMesh());
-};
-
-const selectElement = document.querySelector('select') as HTMLSelectElement;
-
-selectElement.onchange = function() {
-  csg(selectElement.value as BooleanOp);
-};
-
-csg('union');
-
+// Set up Three.js renderer
 const output = document.querySelector('#output')!;
 const renderer = new WebGLRenderer({canvas: output, antialias: true});
 const dim = output.getBoundingClientRect();
@@ -81,31 +56,79 @@ renderer.setAnimationLoop(function(time: number) {
   renderer.render(scene, camera);
 });
 
-// functions to convert between three.js and wasm
+// Create input meshes in Three.js
+const cube = new BoxGeometry(0.2, 0.2, 0.2);
+cube.clearGroups();
+cube.addGroup(0, 18, 0);         // First 6 faces colored by normal
+cube.addGroup(18, Infinity, 1);  // Rest of faces are red
+
+const icosahedron = new IcosahedronGeometry(0.16);
+icosahedron.clearGroups();
+icosahedron.addGroup(0, 30, 0);         // First 10 faces colored by normal
+icosahedron.addGroup(30, Infinity, 2);  // Rest of faces are blue
+
+// Convert Three.js input meshes to Manifolds
+const manifoldCube = new Manifold(geometry2mesh(cube));
+const manifoldIcosahedron = new Manifold(geometry2mesh(icosahedron));
+
+// Set up UI for operations
+type BooleanOp = 'union'|'difference'|'intersection';
+
+function csg(operation: BooleanOp) {
+  result.geometry?.dispose();
+  result.geometry = mesh2geometry(
+      Manifold[operation](manifoldCube, manifoldIcosahedron).getMesh());
+}
+
+csg('union');
+const selectElement = document.querySelector('select') as HTMLSelectElement;
+selectElement.onchange = function() {
+  csg(selectElement.value as BooleanOp);
+};
+
+// Convert Three.js BufferGeometry to Manifold Mesh
 function geometry2mesh(geometry: BufferGeometry) {
+  // Only using position in this sample for simplicity. Can interleave any other
+  // desired attributes here such as UV, normal, etc.
   const vertProperties = geometry.attributes.position.array as Float32Array;
+  // Manifold only uses indexed geometry, so generate an index if necessary.
   const triVerts = geometry.index != null ?
       geometry.index.array as Uint32Array :
       new Uint32Array(vertProperties.length / 3).map((_, idx) => idx);
-  const runIndex = new Uint32Array(geometry.groups.length + 1)
-                       .map((_, idx) => geometry.groups[idx]?.start ?? 0);
-  runIndex.set([triVerts.length], runIndex.length - 1);
+  // Create a triangle run for each group (material).
+  const runIndex =
+      new Uint32Array(geometry.groups.length + 1)
+          .map((_, idx) => geometry.groups[idx]?.start ?? triVerts.length);
+  // Map the materials to ID
   const runOriginalID =
       new Uint32Array(geometry.groups.length)
           .map((_, idx) => ids[geometry.groups[idx].materialIndex!]);
 
   const mesh =
       new Mesh({numProp: 3, vertProperties, triVerts, runIndex, runOriginalID});
+  // Automatically merge vertices with nearly identical positions to create a
+  // Manifold. This only fills in the mergeFromVert and mergeToVert vectors -
+  // these are automatically filled in for any mesh returned by Manifold. These
+  // are necessary because GL drivers require duplicate verts when any
+  // properties change, e.g. a UV boundary or sharp corner.
   mesh.merge();
   return mesh;
 }
 
+// Convert Manifold Mesh to Three.js BufferGeometry
 function mesh2geometry(mesh: Mesh) {
   const geometry = new BufferGeometry();
+  // Assign buffers
   geometry.setAttribute(
       'position', new BufferAttribute(mesh.vertProperties, 3));
   geometry.setIndex(new BufferAttribute(mesh.triVerts, 1));
-
+  // Create a group (material) for each ID. Note that there may be multiple
+  // triangle runs returned with the same ID, though these will always be
+  // sequential since they are sorted by ID. In this example there are two runs
+  // for the MeshNormalMaterial, one corresponding to each input mesh that had
+  // this ID. This allows runTransform to return the total transformation matrix
+  // applied to each triangle run from its input mesh - even after many
+  // consecutive operations.
   let id = mesh.runOriginalID[0];
   let start = mesh.runIndex[0];
   for (let run = 0; run < mesh.numRun; ++run) {
