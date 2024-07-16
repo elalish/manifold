0617d344bcccb5b603d93e94111d6e6a4ea5e6b5
Emmett Lalish <elalish@gmail.com>
2024-07-16 11:10:09 -0700

    fix shadowing warning (#867)

--- src/utilities/include/vec_view.h
+++ src/utilities/include/vec_view.h
@@ -29,7 +29,7 @@ class VecView {
   using Iter = T *;
   using IterC = const T *;
 
-  VecView(T *ptr_, size_t size_) : ptr_(ptr_), size_(size_) {}
+  VecView(T *ptr, size_t size) : ptr_(ptr), size_(size) {}
 
   VecView(const VecView &other) {
     ptr_ = other.ptr_;
