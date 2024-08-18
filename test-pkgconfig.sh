0620985c829f9c9e2bb1e2a436e2198d05a853df
Henner Zeller <h.zeller@acm.org>
2024-08-17 17:19:24 -0700

    Add *.pc file for easier use in non-cmake environments.
    pkg-config is the common way how to find cflags and library flags
    for projects not using cmake.

---
+++ test-pkgconfig.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+mkdir make-consumer
+cd make-consumer
+
+cat <<'EOT' >> Makefile
+CXXFLAGS=$(shell pkg-config --cflags manifold)
+LDFLAGS=$(shell pkg-config --libs manifold)
+
+testing : testing.cpp
+EOT
+
+cat <<EOT >> testing.cpp
+#include <manifold/manifold.h>
+int main() { manifold::Manifold foo; return 0; }
+EOT
+
+make
+./testing
