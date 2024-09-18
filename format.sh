9257734c0fc00034da5960b4e2cc55fbd7068708
Neil Pankey <npankey@gmail.com>
2024-09-17 23:24:30 -0700

    wasm: fix CrossSection slice and project bindings (#930)
    * wasm: fix CrossSection slice and project bindings
    
    * wasm: SwissCheese example to demonstrate `slice`
    
    * format.sh: ignore node_modules
    
    * replace swiss cheese with auger
    
    ---------
    
    Co-authored-by: Emmett Lalish <elalish@google.com>

--- format.sh
+++ format.sh
@@ -11,7 +11,9 @@ $CLANG_FORMAT -i samples/**/*.{h,cpp}
 $CLANG_FORMAT -i test/*.{h,cpp}
 $CLANG_FORMAT -i bindings/*/*.cpp
 $CLANG_FORMAT -i bindings/c/include/manifold/*.h
-$CLANG_FORMAT -i bindings/wasm/**/*.{js,ts,html}
+$CLANG_FORMAT -i bindings/wasm/*.{js,ts}
+$CLANG_FORMAT -i bindings/wasm/examples/*.{js,ts,html}
+$CLANG_FORMAT -i bindings/wasm/examples/public/*.{js,ts}
 $CLANG_FORMAT -i src/*/src/*.{h,cpp}
 $CLANG_FORMAT -i src/*/include/manifold/*.h
 black bindings/python/examples/*.py
