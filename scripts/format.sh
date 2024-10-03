02194f6105c0a1ddf8914b4e57e3211128582415
pca006132 <john.lck40@gmail.com>
2024-10-03 20:07:08 +0800

    update format.sh

--- scripts/format.sh
+++ scripts/format.sh
@@ -6,19 +6,27 @@ CLANG_FORMAT=clang-format
 fi
 
 $CLANG_FORMAT -i extras/*.cpp
-$CLANG_FORMAT -i meshIO/**/*.{h,cpp}
-$CLANG_FORMAT -i samples/**/*.{h,cpp}
+$CLANG_FORMAT -i samples/*/*.{h,cpp}
 $CLANG_FORMAT -i test/*.{h,cpp}
 $CLANG_FORMAT -i bindings/*/*.cpp
 $CLANG_FORMAT -i bindings/c/include/manifold/*.h
 $CLANG_FORMAT -i bindings/wasm/*.{js,ts}
 $CLANG_FORMAT -i bindings/wasm/examples/*.{js,ts,html}
 $CLANG_FORMAT -i bindings/wasm/examples/public/*.{js,ts}
-$CLANG_FORMAT -i src/*/src/*.{h,cpp}
-$CLANG_FORMAT -i src/*/include/manifold/*.h
+$CLANG_FORMAT -i src/*.{h,cpp}
+$CLANG_FORMAT -i src/*/*.cpp
+$CLANG_FORMAT -i include/manifold/*.h
+
 black bindings/python/examples/*.py
 
 for f in $(find -name CMakeLists.txt); do
+  # skip build directories
+  if [[ $f != *build* ]]; then
+    cmake-format -i $f
+  fi
+done
+
+for f in $(find -name '*.cmake.in'); do
   # skip build directories
   if [[ $f != *build* ]]; then
     cmake-format -i $f
