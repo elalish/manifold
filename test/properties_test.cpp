2c627278e285d6d406ade8edc96ab91e6fc7a6c8
Emmett Lalish <elalish@google.com>
2024-11-13 21:59:12 -0800

    added test

--- test/properties_test.cpp
+++ test/properties_test.cpp
@@ -76,6 +76,20 @@ TEST(Properties, Tolerance) {
 #endif
 }
 
+TEST(Properties, ToleranceSphere) {
+  const int n = 100;
+  Manifold sphere = Manifold::Sphere(1, 4 * n);
+  EXPECT_EQ(sphere.NumTri(), 8 * n * n);
+
+  Manifold sphere2 = sphere.SetTolerance(0.01);
+  EXPECT_EQ(sphere2.NumTri(), 16786);
+  EXPECT_NEAR(sphere.Volume(), sphere2.Volume(), 0.02);
+  EXPECT_NEAR(sphere.SurfaceArea(), sphere2.SurfaceArea(), 0.01);
+#ifdef MANIFOLD_EXPORT
+  if (options.exportModels) ExportMesh("sphere.glb", sphere2.GetMeshGL(), {});
+#endif
+}
+
 /**
  * Curvature is the inverse of the radius of curvature, and signed such that
  * positive is convex and negative is concave. There are two orthogonal
