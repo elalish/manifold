1747102d59b61841cfe1136bb25cc5df9c6b9fb5
pca006132 <pca006132@users.noreply.github.com>
2024-12-07 07:51:38 +0000

    deploy: ec0bbca3d92a5f10d1cf1898678bc4bd1dc97b82

---
+++ docs/html/index.js
@@ -0,0 +1,24 @@
+var index =
+[
+    [ "Users", "index.html#autotoc_md1", [
+      [ "Bindings & Packages", "index.html#autotoc_md2", null ]
+    ] ],
+    [ "Frontend Sandboxes", "index.html#autotoc_md3", [
+      [ "ManifoldCAD.org", "index.html#autotoc_md4", null ],
+      [ "Python Colab Example", "index.html#autotoc_md5", null ]
+    ] ],
+    [ "Manifold Library", "index.html#autotoc_md6", [
+      [ "Dependencies", "index.html#autotoc_md7", null ],
+      [ "3D Formats", "index.html#autotoc_md8", null ]
+    ] ],
+    [ "Building", "index.html#autotoc_md9", [
+      [ "WASM", "index.html#autotoc_md10", null ],
+      [ "Python", "index.html#autotoc_md11", null ],
+      [ "Windows Shenanigans", "index.html#autotoc_md12", null ]
+    ] ],
+    [ "Contributing", "index.html#autotoc_md13", [
+      [ "Formatting", "index.html#autotoc_md14", null ],
+      [ "Profiling", "index.html#autotoc_md15", null ]
+    ] ],
+    [ "About the author", "index.html#autotoc_md16", null ]
+];
---
+++ docs/html/search/groups_b.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['reductions_0',['Reductions',['../group__reductions.html',1,'']]],
+  ['relational_1',['Relational',['../group__relational.html',1,'']]]
+];
