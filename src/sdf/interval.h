05e64d7b32d6c5e5df34dd386916fee92cfadd2c
pca006132 <john.lck40@gmail.com>
2024-12-22 22:21:18 +0800

    init sdf eval stuff

---
+++ src/sdf/interval.h
@@ -0,0 +1,202 @@
+// Copyright 2024 The Manifold Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#pragma once
+
+#include <algorithm>
+#include <limits>
+
+#include "manifold/common.h"
+
+namespace manifold::sdf {
+
+// not really a precise implementation...
+template <typename Domain>
+struct Interval {
+  Domain lower;
+  Domain upper;
+
+  static Interval constant(Domain v) { return {v, v}; }
+
+  constexpr Interval operator+(const Interval &other) const {
+    return {lower + other.lower, upper + other.upper};
+  }
+
+  constexpr Interval operator-() const { return {-upper, -lower}; }
+
+  constexpr Interval operator-(const Interval &other) const {
+    return *this + (-other);
+  }
+
+  Interval operator*(const Interval &other) const {
+    Domain a1b1 = lower * other.lower;
+    Domain a2b2 = upper * other.upper;
+    // we can write more "fast paths", but at some point it will become slower
+    // than just going the general path...
+    if (lower >= 0.0 && other.lower >= 0.0)
+      return {a1b1, a2b2};
+    else if (upper <= 0.0 && other.upper <= 0.0)
+      return {a2b2, a1b1};
+
+    Domain a1b2 = lower * other.upper;
+    Domain a2b1 = upper * other.lower;
+    return {std::min(std::min(a1b1, a1b2), std::min(a2b1, a2b2)),
+            std::max(std::max(a1b1, a1b2), std::max(a2b1, a2b2))};
+  }
+
+  Interval operator*(double d) const {
+    if (d > 0) return {lower * d, upper * d};
+    return {upper * d, lower * d};
+  }
+
+  Interval operator/(const Interval &other) const {
+    if (other.is_const()) return *this / other.lower;
+    constexpr Domain zero = static_cast<Domain>(0);
+    constexpr Domain infty = std::numeric_limits<Domain>::infinity();
+    Interval reci;
+    if (other.lower >= zero || other.upper <= zero) {
+      reci.lower = other.upper == zero ? -infty : (1 / other.upper);
+      reci.upper = other.lower == zero ? infty : (1 / other.lower);
+    } else {
+      reci.lower = -infty;
+      reci.upper = infty;
+    }
+    return *this * reci;
+  }
+
+  Interval operator/(double d) const {
+    if (d > 0) return {lower / d, upper / d};
+    return {upper / d, lower / d};
+  }
+
+  constexpr bool is_const() const { return lower == upper; }
+
+  constexpr Interval operator==(const Interval &other) const {
+    if (is_const() && other.is_const() && lower == other.lower)
+      return constant(1);  // must be equal
+    if (lower > other.upper || upper < other.lower)
+      return constant(0);  // disjoint, cannot possibly be equal
+    return {0, 1};
+  }
+
+  constexpr bool operator==(double d) const { return is_const() && lower == d; }
+
+  constexpr Interval operator>(const Interval &other) const {
+    if (lower > other.upper) return constant(1);
+    if (upper < other.lower) return constant(0);
+    return {0, 1};
+  }
+
+  constexpr Interval operator<(const Interval &other) const {
+    if (upper < other.lower) return constant(1);
+    if (lower > other.upper) return constant(0);
+    return {0, 1};
+  }
+
+  constexpr Interval min(const Interval &other) const {
+    return {std::min(lower, other.lower), std::min(upper, other.upper)};
+  }
+
+  constexpr Interval max(const Interval &other) const {
+    return {std::max(lower, other.lower), std::max(upper, other.upper)};
+  }
+
+  constexpr Interval merge(const Interval &other) const {
+    return {std::min(lower, other.lower), std::max(upper, other.upper)};
+  }
+
+  template <typename F>
+  constexpr Interval monotone_map(F f) const {
+    if (is_const()) return constant(f(lower));
+    return {f(lower), f(upper)};
+  }
+
+  template <typename F>
+  constexpr Interval antimonotone_map(F f) const {
+    if (is_const()) return constant(f(lower));
+    return {f(upper), f(lower)};
+  }
+
+  constexpr Interval abs() const {
+    if (lower >= 0) return *this;
+    if (upper <= 0) return {-upper, -lower};
+    return {0.0, std::max(-lower, upper)};
+  }
+
+  constexpr Interval mod(double m) const {
+    // FIXME: cannot deal with negative m right now...
+    Domain diff = std::fmod(lower, m);
+    if (diff < 0) diff += m;
+    Domain cycle_min = lower - diff;
+    // may be disjoint intervals, but we don't deal with that...
+    if (upper - cycle_min >= m) return {0.0, m};
+    return {diff, upper - cycle_min};
+  }
+
+  constexpr Interval logical_and(const Interval &other) const {
+    return {lower == 0.0 || other.lower == 0.0 ? 0.0 : 1.0,
+            upper == 1.0 && other.upper == 1.0 ? 1.0 : 0.0};
+  }
+
+  constexpr Interval logical_or(const Interval &other) const {
+    return {lower == 0.0 && other.lower == 0.0 ? 0.0 : 1.0,
+            upper == 1.0 || other.upper == 1.0 ? 1.0 : 0.0};
+  }
+
+  constexpr Interval sin() const {
+    if (is_const()) return constant(std::sin(lower));
+    // largely similar to cos
+    int64_t min_pis = static_cast<int64_t>(std::floor((lower - kHalfPi) / kPi));
+    int64_t max_pis = static_cast<int64_t>(std::floor((upper - kHalfPi) / kPi));
+
+    bool not_cross_pos_1 =
+        (min_pis % 2 == 0) ? max_pis - min_pis <= 1 : max_pis == min_pis;
+    bool not_cross_neg_1 =
+        (min_pis % 2 == 0) ? max_pis == min_pis : max_pis - min_pis <= 1;
+
+    Domain new_min =
+        not_cross_neg_1 ? std::min(std::sin(lower), std::sin(upper)) : -1.0;
+    Domain new_max =
+        not_cross_pos_1 ? std::max(std::sin(lower), std::sin(upper)) : 1.0;
+    return {new_min, new_max};
+  }
+
+  constexpr Interval cos() const {
+    if (is_const()) return constant(std::cos(lower));
+    int64_t min_pis = static_cast<int64_t>(std::floor(lower / kPi));
+    int64_t max_pis = static_cast<int64_t>(std::floor(upper / kPi));
+
+    bool not_cross_pos_1 =
+        (min_pis % 2 == 0) ? max_pis - min_pis <= 1 : max_pis == min_pis;
+    bool not_cross_neg_1 =
+        (min_pis % 2 == 0) ? max_pis == min_pis : max_pis - min_pis <= 1;
+
+    Domain new_min =
+        not_cross_neg_1 ? std::min(std::cos(lower), std::cos(upper)) : -1.0;
+    Domain new_max =
+        not_cross_pos_1 ? std::max(std::cos(lower), std::cos(upper)) : 1.0;
+    return {new_min, new_max};
+  }
+
+  constexpr Interval tan() const {
+    if (is_const()) return constant(std::tan(lower));
+    int64_t min_pis = static_cast<int64_t>(std::floor((lower + kHalfPi) / kPi));
+    int64_t max_pis = static_cast<int64_t>(std::floor((upper + kHalfPi) / kPi));
+    if (min_pis != max_pis)
+      return {-std::numeric_limits<Domain>::infinity(),
+              std::numeric_limits<Domain>::infinity()};
+    return monotone_map([](Domain x) { return std::tan(x); });
+  }
+};
+
+}  // namespace manifold::sdf
---
+++ src/sdf/tape.h
@@ -0,0 +1,277 @@
+// Copyright 2024 The Manifold Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#pragma once
+
+#include <cmath>
+#include <cstdint>
+#include <cstdio>
+#include <limits>
+
+#include "interval.h"
+#include "manifold/vec_view.h"
+
+namespace manifold::sdf {
+
+enum class OpCode : uint8_t {
+  NOP,
+  RETURN,
+
+  // unary operations
+  ABS,
+  NEG,
+  EXP,
+  LOG,
+  SQRT,
+  FLOOR,
+  CEIL,
+  ROUND,
+  SIN,
+  COS,
+  TAN,
+  ASIN,
+  ACOS,
+  ATAN,
+
+  // binary operations,
+  DIV,
+  MOD,
+  MIN,
+  MAX,
+  EQ,
+  GT,
+  AND,
+  OR,
+
+  // fast binary operations
+  ADD,
+  SUB,
+  MUL,
+  // ternary operations
+  FMA,
+  CHOICE,
+};
+
+template <typename Domain>
+struct EvalContext {
+  VecView<const uint8_t> tape;
+  VecView<Domain> buffer;
+
+  Domain& operand(uint32_t x) { return buffer[x]; }
+
+  static Domain handle_unary(OpCode op, Domain operand);
+
+  static Domain handle_binary(OpCode op, Domain lhs, Domain rhs);
+
+  static Domain handle_choice(Domain cond, Domain lhs, Domain rhs);
+
+  Domain eval() {
+    size_t i = 0;
+    while (1) {
+      OpCode current = static_cast<OpCode>(tape[i]);
+      // fast binary/ternary operations
+      if (current >= OpCode::ADD) {
+        // loop is needed to force the compiler to use a tight code layout
+        do {
+          size_t result = tape[i + 1];
+          Domain lhs = operand(tape[i + 2]);
+          Domain rhs = operand(tape[i + 3]);
+          i += 4;
+          if (current <= OpCode::MUL) {
+            if (current == OpCode::ADD)
+              operand(result) = lhs + rhs;
+            else if (current == OpCode::SUB)
+              operand(result) = lhs - rhs;
+            else
+              operand(result) = lhs * rhs;
+          } else {
+            Domain z = operand(tape[i++]);
+            if (current == OpCode::FMA)
+              operand(result) = lhs * rhs + z;
+            else
+              operand(result) = handle_choice(lhs, rhs, z);
+          }
+          current = static_cast<OpCode>(tape[i]);
+        } while (current >= OpCode::ADD);
+      }
+      if (current >= OpCode::DIV) {
+        Domain lhs = operand(tape[i + 2]);
+        Domain rhs = operand(tape[i + 3]);
+        operand(tape[i + 1]) = handle_binary(current, lhs, rhs);
+        i += 4;
+      } else if (current >= OpCode::ABS) {
+        Domain x = operand(tape[i + 2]);
+        operand(tape[i + 1]) = handle_unary(current, x);
+        i += 3;
+      } else if (current == OpCode::RETURN) {
+        return operand(tape[i + 1]);
+      } else {
+        i += 1;
+      }
+    }
+  }
+};
+
+template <>
+inline double EvalContext<double>::handle_unary(OpCode op, double x) {
+  switch (op) {
+    case OpCode::ABS:
+      return std::abs(x);
+    case OpCode::NEG:
+      return -x;
+    case OpCode::EXP:
+      return std::exp(x);
+    case OpCode::LOG:
+      return std::log(x);
+    case OpCode::SQRT:
+      return std::sqrt(x);
+    case OpCode::FLOOR:
+      return std::floor(x);
+    case OpCode::CEIL:
+      return std::ceil(x);
+    case OpCode::ROUND:
+      return std::round(x);
+    case OpCode::SIN:
+      return std::sin(x);
+    case OpCode::COS:
+      return std::cos(x);
+    case OpCode::TAN:
+      return std::tan(x);
+    case OpCode::ASIN:
+      return std::asin(x);
+    case OpCode::ACOS:
+      return std::acos(x);
+    case OpCode::ATAN:
+      return std::atan(x);
+    default:
+      return 0.0;
+  }
+}
+
+template <>
+inline double EvalContext<double>::handle_binary(OpCode op, double lhs,
+                                                 double rhs) {
+  switch (op) {
+    case OpCode::DIV:
+      return lhs / rhs;
+    case OpCode::MOD:
+      // FIXME: negative rhs???
+      return std::fmod(std::fmod(lhs, rhs) + rhs, rhs);
+    case OpCode::MIN:
+      return std::min(lhs, rhs);
+    case OpCode::MAX:
+      return std::max(lhs, rhs);
+    case OpCode::EQ:
+      return lhs == rhs ? 1.0 : 0.0;
+    case OpCode::GT:
+      return lhs > rhs ? 1.0 : 0.0;
+    case OpCode::AND:
+      return (lhs == 1.0 && rhs == 1.0) ? 1.0 : 0.0;
+    case OpCode::OR:
+      return (lhs == 1.0 || rhs == 1.0) ? 1.0 : 0.0;
+    default:
+      return 0;
+  }
+}
+
+template <>
+inline double EvalContext<double>::handle_choice(double cond, double lhs,
+                                                 double rhs) {
+  if (cond == 1.0) return lhs;
+  return rhs;
+}
+
+template <>
+inline Interval<double> EvalContext<Interval<double>>::handle_unary(
+    OpCode op, Interval<double> x) {
+  constexpr double infty = std::numeric_limits<double>::infinity();
+  switch (op) {
+    case OpCode::ABS:
+      return x.abs();
+    case OpCode::NEG:
+      return -x;
+    case OpCode::EXP:
+      return x.monotone_map([](double v) { return std::exp(v); });
+    case OpCode::LOG:
+      return x.monotone_map(
+          [infty](double v) { return v > 0.0 ? std::log(v) : -infty; });
+    case OpCode::SQRT:
+      return x.monotone_map(
+          [infty](double v) { return v >= 0.0 ? std::sqrt(v) : 0.0; });
+    case OpCode::FLOOR:
+      return x.monotone_map([](double v) { return std::floor(v); });
+    case OpCode::CEIL:
+      return x.monotone_map([](double v) { return std::ceil(v); });
+    case OpCode::ROUND:
+      return x.monotone_map([](double v) { return std::round(v); });
+    case OpCode::SIN:
+      return x.sin();
+    case OpCode::COS:
+      return x.cos();
+    case OpCode::TAN:
+      return x.tan();
+    case OpCode::ASIN:
+      return x.monotone_map([infty](double v) {
+        return v < -1.0 ? -infty : v > 1.0 ? infty : std::asin(v);
+      });
+    case OpCode::ACOS:
+      return x.antimonotone_map([infty](double v) {
+        return v < -1.0 ? infty : v > 1.0 ? -infty : std::acos(v);
+      });
+    case OpCode::ATAN:
+      return x.monotone_map([](double v) { return std::atan(v); });
+    default:
+      return {0.0, 0.0};
+  }
+}
+
+template <>
+inline Interval<double> EvalContext<Interval<double>>::handle_binary(
+    OpCode op, Interval<double> lhs, Interval<double> rhs) {
+  switch (op) {
+    case OpCode::DIV:
+      return lhs / rhs;
+    case OpCode::MOD:
+      return lhs.is_const()
+                 ? lhs.mod(rhs.lower)
+                 : (rhs.lower < 0
+                        ? Interval<double>{rhs.lower, std::max(0.0, rhs.upper)}
+                        : Interval<double>{0, rhs.upper});
+    case OpCode::MIN:
+      return lhs.min(rhs);
+    case OpCode::MAX:
+      return lhs.max(rhs);
+    case OpCode::EQ:
+      return lhs == rhs;
+    case OpCode::GT:
+      return lhs > rhs;
+    case OpCode::AND:
+      return lhs.logical_and(rhs);
+    case OpCode::OR:
+      return lhs.logical_or(rhs);
+    default:
+      return {0.0, 0.0};
+  }
+}
+
+template <>
+inline Interval<double> EvalContext<Interval<double>>::handle_choice(
+    Interval<double> cond, Interval<double> lhs, Interval<double> rhs) {
+  if (cond.is_const()) {
+    if (cond.lower == 1.0) return lhs;
+    return rhs;
+  }
+  return lhs.merge(rhs);
+}
+
+}  // namespace manifold::sdf
