3b3f54e4d3dfa56f454ea5266377b742ad9bd7bb
pca006132 <john.lck40@gmail.com>
2024-12-24 11:13:01 +0800

    fix typescript

--- bindings/wasm/examples/worker.test.ts
+++ bindings/wasm/examples/worker.test.ts
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 import {WebIO} from '@gltf-transform/core';
-import assert from 'node:assert';
+import {strict as assert} from 'assert';
 import {afterEach, expect, suite, test} from 'vitest';
 
 import {readMesh, setupIO} from './gltf-io';
@@ -29,8 +29,8 @@ async function runExample(name: string) {
   const result = await evaluateCADToModel(code);
   cleanup();
   assert.ok(result?.glbURL);
-  const docIn = await io.read(result.glbURL);
-  URL.revokeObjectURL(result.glbURL);
+  const docIn = await io.read(result?.glbURL);
+  URL.revokeObjectURL(result?.glbURL);
   const nodes = docIn.getRoot().listNodes();
   for (const node of nodes) {
     const docMesh = node.getMesh();
@@ -55,71 +55,71 @@ afterEach(async () => {await new Promise(resolve => setTimeout(resolve, 500))})
 suite('Examples', () => {
   test('Intro', async () => {
     const result = await runExample('Intro');
-    expect(result.genus).to.equal(5, 'Genus');
-    expect(result.volume).to.be.closeTo(203164, 1, 'Volume');
-    expect(result.surfaceArea).to.be.closeTo(62046, 1, 'Surface Area');
+    expect(result?.genus).to.equal(5, 'Genus');
+    expect(result?.volume).to.be.closeTo(203164, 1, 'Volume');
+    expect(result?.surfaceArea).to.be.closeTo(62046, 1, 'Surface Area');
   });
 
   test('Auger', async () => {
     const result = await runExample('Auger');
-    expect(result.genus).to.equal(0, 'Genus');
-    expect(result.volume).to.be.closeTo(16842, 1, 'Volume');
-    expect(result.surfaceArea).to.be.closeTo(10519, 1, 'Surface Area');
+    expect(result?.genus).to.equal(0, 'Genus');
+    expect(result?.volume).to.be.closeTo(16842, 1, 'Volume');
+    expect(result?.surfaceArea).to.be.closeTo(10519, 1, 'Surface Area');
   });
 
   test('Tetrahedron Puzzle', async () => {
     const result = await runExample('Tetrahedron Puzzle');
-    expect(result.genus).to.equal(0, 'Genus');
-    expect(result.volume).to.be.closeTo(7240, 1, 'Volume');
-    expect(result.surfaceArea).to.be.closeTo(3235, 1, 'Surface Area');
+    expect(result?.genus).to.equal(0, 'Genus');
+    expect(result?.volume).to.be.closeTo(7240, 1, 'Volume');
+    expect(result?.surfaceArea).to.be.closeTo(3235, 1, 'Surface Area');
   });
 
   test('Rounded Frame', async () => {
     const result = await runExample('Rounded Frame');
-    expect(result.genus).to.equal(5, 'Genus');
-    expect(result.volume).to.be.closeTo(270807, 10, 'Volume');
-    expect(result.surfaceArea).to.be.closeTo(74599, 1, 'Surface Area');
+    expect(result?.genus).to.equal(5, 'Genus');
+    expect(result?.volume).to.be.closeTo(270807, 10, 'Volume');
+    expect(result?.surfaceArea).to.be.closeTo(74599, 1, 'Surface Area');
   });
 
   test('Heart', async () => {
     const result = await runExample('Heart');
-    expect(result.genus).to.equal(0, 'Genus');
-    expect(result.volume).to.be.closeTo(3.342, 0.001, 'Volume');
-    expect(result.surfaceArea).to.be.closeTo(11.51, 0.01, 'Surface Area');
+    expect(result?.genus).to.equal(0, 'Genus');
+    expect(result?.volume).to.be.closeTo(3.342, 0.001, 'Volume');
+    expect(result?.surfaceArea).to.be.closeTo(11.51, 0.01, 'Surface Area');
   });
 
   test('Scallop', async () => {
     const result = await runExample('Scallop');
-    expect(result.genus).to.equal(0, 'Genus');
-    expect(result.volume).to.be.closeTo(39900, 100, 'Volume');
-    expect(result.surfaceArea).to.be.closeTo(7930, 10, 'Surface Area');
+    expect(result?.genus).to.equal(0, 'Genus');
+    expect(result?.volume).to.be.closeTo(39900, 100, 'Volume');
+    expect(result?.surfaceArea).to.be.closeTo(7930, 10, 'Surface Area');
   });
 
   test('Torus Knot', async () => {
     const result = await runExample('Torus Knot');
-    expect(result.genus).to.equal(1, 'Genus');
-    expect(result.volume).to.be.closeTo(20786, 1, 'Volume');
-    expect(result.surfaceArea).to.be.closeTo(11177, 1, 'Surface Area');
+    expect(result?.genus).to.equal(1, 'Genus');
+    expect(result?.volume).to.be.closeTo(20786, 1, 'Volume');
+    expect(result?.surfaceArea).to.be.closeTo(11177, 1, 'Surface Area');
   });
 
   test('Menger Sponge', async () => {
     const result = await runExample('Menger Sponge');
-    expect(result.genus).to.equal(729, 'Genus');
-    expect(result.volume).to.be.closeTo(203222, 10, 'Volume');
-    expect(result.surfaceArea).to.be.closeTo(130475, 10, 'Surface Area');
+    expect(result?.genus).to.equal(729, 'Genus');
+    expect(result?.volume).to.be.closeTo(203222, 10, 'Volume');
+    expect(result?.surfaceArea).to.be.closeTo(130475, 10, 'Surface Area');
   });
 
   test('Stretchy Bracelet', async () => {
     const result = await runExample('Stretchy Bracelet');
-    expect(result.genus).to.equal(1, 'Genus');
-    expect(result.volume).to.be.closeTo(3992, 1, 'Volume');
-    expect(result.surfaceArea).to.be.closeTo(22267, 1, 'Surface Area');
+    expect(result?.genus).to.equal(1, 'Genus');
+    expect(result?.volume).to.be.closeTo(3992, 1, 'Volume');
+    expect(result?.surfaceArea).to.be.closeTo(22267, 1, 'Surface Area');
   });
 
   test('Gyroid Module', async () => {
     const result = await runExample('Gyroid Module');
-    expect(result.genus).to.equal(15, 'Genus');
-    expect(result.volume).to.be.closeTo(4175, 1, 'Volume');
-    expect(result.surfaceArea).to.be.closeTo(5645, 1, 'Surface Area');
+    expect(result?.genus).to.equal(15, 'Genus');
+    expect(result?.volume).to.be.closeTo(4175, 1, 'Volume');
+    expect(result?.surfaceArea).to.be.closeTo(5645, 1, 'Surface Area');
   });
 });
