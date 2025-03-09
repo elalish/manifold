b8944362821c3b871e20be8641fcaa6a1a5c0227
pca006132 <pca006132@users.noreply.github.com>
2025-03-09 09:32:10 +0000

    deploy: d4439bccec89d0fb5e7997c06d27c3dc9fb042a5

---
+++ docs/html/group___triangulation.js
@@ -0,0 +1,5 @@
+var group___triangulation =
+[
+    [ "TriangulateIdx", "group___triangulation.html#gacb67114040ef7ce7a762bf52285b77a9", null ],
+    [ "Triangulate", "group___triangulation.html#gad94a4b0c8fcbba40756f72d741adf160", null ]
+];
---
+++ docs/html/search/all_5.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['debug_0',['Debug',['../group___debug.html',1,'']]],
+  ['decompose_1',['Decompose',['../classmanifold_1_1_manifold.html#a7f20b130779680156ce0d28d91e5d106',1,'manifold::Manifold::Decompose()'],['../classmanifold_1_1_cross_section.html#a48f77ce6390caee13a94f681893fb65c',1,'manifold::CrossSection::Decompose()']]],
+  ['degrees_2',['degrees',['../group___scalar.html#ga9fdb4379a097954f1a0b9bee6aadc812',1,'manifold']]],
+  ['dependencies_3',['Dependencies',['../index.html#autotoc_md7',1,'']]],
+  ['distance_4',['distance',['../group__vec__algebra.html#gafa2e270a2bd944eef65542243670f65f',1,'linalg']]],
+  ['distance2_5',['distance2',['../group__vec__algebra.html#ga27cbd479fb849af7f57ada1e551de768',1,'linalg']]],
+  ['doesoverlap_6',['DoesOverlap',['../structmanifold_1_1_box.html#a75bb41be7ddc6e4b77b3456418245aa8',1,'manifold::Box::DoesOverlap(const Box &amp;box) const'],['../structmanifold_1_1_box.html#a94dbcc8d35d24d54a8de1d1643307582',1,'manifold::Box::DoesOverlap(vec3 p) const'],['../structmanifold_1_1_rect.html#ac2bf0d9c3c91b07fd81499a3cbee4c75',1,'manifold::Rect::DoesOverlap()']]],
+  ['dot_7',['dot',['../group__vec__algebra.html#ga7ac5d3ed262611340e95129e540eaf0a',1,'linalg']]],
+  ['drive_201vxrfyhpshzgubl9tewzceovlpxrpq5j5_20usp_20sharing_20python_20colab_20example_20a_8',['&lt;a href=&quot;https://colab.research.google.com/drive/1VxrFYHPSHZgUbl9TeWzCeovlpXrPQ5J5?usp=sharing&quot; &gt;Python Colab Example&lt;/a&gt;',['../index.html#autotoc_md5',1,'']]]
+];
