ea2d38304f0992307dd9efaf84202b98adb2948e
elalish <elalish@users.noreply.github.com>
2025-05-14 19:58:41 +0000

    deploy: 551e1a12cf9162675e84e759f783757d9095ae47

---
+++ docs/html/group__quaternion__rotation.js
@@ -0,0 +1,15 @@
+var group__quaternion__rotation =
+[
+    [ "qxdir", "group__quaternion__rotation.html#gabc2df3058b0a3cfafca78cff18c8bb68", null ],
+    [ "qydir", "group__quaternion__rotation.html#gae7f3ec818c95d8d1b511beaf163f9c66", null ],
+    [ "qzdir", "group__quaternion__rotation.html#ga5d928fac4d965550405b44c49d0c5230", null ],
+    [ "qmat", "group__quaternion__rotation.html#ga167e0c6be5787a35d064e5993c9c6fc0", null ],
+    [ "qrot", "group__quaternion__rotation.html#gacd822cab55e4aa6beec4351482017efe", null ],
+    [ "qangle", "group__quaternion__rotation.html#ga2b3456b027915b1aaefe7c915a64299e", null ],
+    [ "qaxis", "group__quaternion__rotation.html#gaa608cfc242186a21544a004737a0d877", null ],
+    [ "qnlerp", "group__quaternion__rotation.html#ga90364ed59c098b67355ff89a98c225e5", null ],
+    [ "qslerp", "group__quaternion__rotation.html#ga20429b3fcef9b54534c23975fc72f654", null ],
+    [ "rotation_quat", "group__quaternion__rotation.html#gaa6292de23f466f4e0c0da188378947d1", null ],
+    [ "rotation_quat", "group__quaternion__rotation.html#ga8111957451021af7c66d8d37e5aea3c9", null ],
+    [ "rotation_quat", "group__quaternion__rotation.html#ga125d7bb18e01e1f7247cb2b90886cb96", null ]
+];
