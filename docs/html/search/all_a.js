edf1645c75fabc49814bea6760cc26c559bd731f
elalish <elalish@users.noreply.github.com>
2025-05-29 09:56:01 +0000

    deploy: 023bb59be97e1573f2055eb1aef066cfee318384

---
+++ docs/html/search/all_a.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['identity_0',['Identity',['../group__identity.html',1,'']]],
+  ['identity_5ft_1',['identity_t',['../structlinalg_1_1identity__t.html',1,'linalg']]],
+  ['idx_2',['idx',['../group___structs.html#ae40354a1051342eb5a9db005715dcfa9',1,'manifold::PolyVert']]],
+  ['importmesh_3',['ImportMesh',['../group___mesh_i_o.html#ga8a512673f3c1f1d488418b1d81d2b952',1,'manifold']]],
+  ['intermediatechecks_4',['intermediateChecks',['../group___debug.html#ae8dc7c8ba98011f6f2dbe1caddf2a6fb',1,'manifold::ExecutionParams']]],
+  ['isempty_5',['IsEmpty',['../classmanifold_1_1_manifold.html#a8e12342fc420701fbffd97025421575a',1,'manifold::Manifold::IsEmpty()'],['../classmanifold_1_1_cross_section.html#a8e12342fc420701fbffd97025421575a',1,'manifold::CrossSection::IsEmpty()'],['../structmanifold_1_1_rect.html#a334a154c146845a18658fc659a9da461',1,'manifold::Rect::IsEmpty()']]],
+  ['isfinite_6',['IsFinite',['../structmanifold_1_1_box.html#abcd842f1b49dd6ac4ca84d39d0e74b01',1,'manifold::Box::IsFinite()'],['../structmanifold_1_1_rect.html#abcd842f1b49dd6ac4ca84d39d0e74b01',1,'manifold::Rect::IsFinite()']]],
+  ['iterators_7',['Iterators',['../group__iterators.html',1,'']]]
+];
---
+++ docs/html/search/classes_2.js
@@ -0,0 +1,20 @@
+var searchData=
+[
+  ['clamp_0',['clamp',['../structlinalg_1_1detail_1_1clamp.html',1,'linalg::detail']]],
+  ['converter_1',['converter',['../structlinalg_1_1converter.html',1,'linalg']]],
+  ['converter_3c_20mat_3c_20t_2c_201_2c_201_20_3e_2c_20identity_5ft_20_3e_2',['converter&lt; mat&lt; T, 1, 1 &gt;, identity_t &gt;',['../structlinalg_1_1converter_3_01mat_3_01_t_00_011_00_011_01_4_00_01identity__t_01_4.html',1,'linalg']]],
+  ['converter_3c_20mat_3c_20t_2c_202_2c_202_20_3e_2c_20identity_5ft_20_3e_3',['converter&lt; mat&lt; T, 2, 2 &gt;, identity_t &gt;',['../structlinalg_1_1converter_3_01mat_3_01_t_00_012_00_012_01_4_00_01identity__t_01_4.html',1,'linalg']]],
+  ['converter_3c_20mat_3c_20t_2c_202_2c_203_20_3e_2c_20identity_5ft_20_3e_4',['converter&lt; mat&lt; T, 2, 3 &gt;, identity_t &gt;',['../structlinalg_1_1converter_3_01mat_3_01_t_00_012_00_013_01_4_00_01identity__t_01_4.html',1,'linalg']]],
+  ['converter_3c_20mat_3c_20t_2c_203_2c_203_20_3e_2c_20identity_5ft_20_3e_5',['converter&lt; mat&lt; T, 3, 3 &gt;, identity_t &gt;',['../structlinalg_1_1converter_3_01mat_3_01_t_00_013_00_013_01_4_00_01identity__t_01_4.html',1,'linalg']]],
+  ['converter_3c_20mat_3c_20t_2c_203_2c_204_20_3e_2c_20identity_5ft_20_3e_6',['converter&lt; mat&lt; T, 3, 4 &gt;, identity_t &gt;',['../structlinalg_1_1converter_3_01mat_3_01_t_00_013_00_014_01_4_00_01identity__t_01_4.html',1,'linalg']]],
+  ['converter_3c_20mat_3c_20t_2c_204_2c_204_20_3e_2c_20identity_5ft_20_3e_7',['converter&lt; mat&lt; T, 4, 4 &gt;, identity_t &gt;',['../structlinalg_1_1converter_3_01mat_3_01_t_00_014_00_014_01_4_00_01identity__t_01_4.html',1,'linalg']]],
+  ['converter_3c_20std_3a_3aarray_3c_20t_2c_201_20_3e_2c_20vec_3c_20t_2c_201_20_3e_20_3e_8',['converter&lt; std::array&lt; T, 1 &gt;, vec&lt; T, 1 &gt; &gt;',['../structlinalg_1_1converter_3_01std_1_1array_3_01_t_00_011_01_4_00_01vec_3_01_t_00_011_01_4_01_4.html',1,'linalg']]],
+  ['converter_3c_20std_3a_3aarray_3c_20t_2c_202_20_3e_2c_20vec_3c_20t_2c_202_20_3e_20_3e_9',['converter&lt; std::array&lt; T, 2 &gt;, vec&lt; T, 2 &gt; &gt;',['../structlinalg_1_1converter_3_01std_1_1array_3_01_t_00_012_01_4_00_01vec_3_01_t_00_012_01_4_01_4.html',1,'linalg']]],
+  ['converter_3c_20std_3a_3aarray_3c_20t_2c_203_20_3e_2c_20vec_3c_20t_2c_203_20_3e_20_3e_10',['converter&lt; std::array&lt; T, 3 &gt;, vec&lt; T, 3 &gt; &gt;',['../structlinalg_1_1converter_3_01std_1_1array_3_01_t_00_013_01_4_00_01vec_3_01_t_00_013_01_4_01_4.html',1,'linalg']]],
+  ['converter_3c_20std_3a_3aarray_3c_20t_2c_204_20_3e_2c_20vec_3c_20t_2c_204_20_3e_20_3e_11',['converter&lt; std::array&lt; T, 4 &gt;, vec&lt; T, 4 &gt; &gt;',['../structlinalg_1_1converter_3_01std_1_1array_3_01_t_00_014_01_4_00_01vec_3_01_t_00_014_01_4_01_4.html',1,'linalg']]],
+  ['converter_3c_20vec_3c_20t_2c_201_20_3e_2c_20std_3a_3aarray_3c_20t_2c_201_20_3e_20_3e_12',['converter&lt; vec&lt; T, 1 &gt;, std::array&lt; T, 1 &gt; &gt;',['../structlinalg_1_1converter_3_01vec_3_01_t_00_011_01_4_00_01std_1_1array_3_01_t_00_011_01_4_01_4.html',1,'linalg']]],
+  ['converter_3c_20vec_3c_20t_2c_202_20_3e_2c_20std_3a_3aarray_3c_20t_2c_202_20_3e_20_3e_13',['converter&lt; vec&lt; T, 2 &gt;, std::array&lt; T, 2 &gt; &gt;',['../structlinalg_1_1converter_3_01vec_3_01_t_00_012_01_4_00_01std_1_1array_3_01_t_00_012_01_4_01_4.html',1,'linalg']]],
+  ['converter_3c_20vec_3c_20t_2c_203_20_3e_2c_20std_3a_3aarray_3c_20t_2c_203_20_3e_20_3e_14',['converter&lt; vec&lt; T, 3 &gt;, std::array&lt; T, 3 &gt; &gt;',['../structlinalg_1_1converter_3_01vec_3_01_t_00_013_01_4_00_01std_1_1array_3_01_t_00_013_01_4_01_4.html',1,'linalg']]],
+  ['converter_3c_20vec_3c_20t_2c_204_20_3e_2c_20std_3a_3aarray_3c_20t_2c_204_20_3e_20_3e_15',['converter&lt; vec&lt; T, 4 &gt;, std::array&lt; T, 4 &gt; &gt;',['../structlinalg_1_1converter_3_01vec_3_01_t_00_014_01_4_00_01std_1_1array_3_01_t_00_014_01_4_01_4.html',1,'linalg']]],
+  ['crosssection_16',['CrossSection',['../classmanifold_1_1_cross_section.html',1,'manifold']]]
+];
