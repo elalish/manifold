8c90d59f8b6f25f10acee7559329688a14035c11
pca006132 <pca006132@users.noreply.github.com>
2025-06-05 04:31:42 +0000

    deploy: 90cab6e2bb91757f3d98051f33d8780527a9f3d6

---
+++ docs/html/search/all_14.js
@@ -0,0 +1,19 @@
+var searchData=
+[
+  ['tetpuzzle_0',['TetPuzzle',['../group___samples.html#gaa69f000001c833bf1534f35c19a7e8b0',1,'manifold']]],
+  ['tetrahedron_1',['Tetrahedron',['../classmanifold_1_1_manifold.html#afbd42733f05fb87dec1b3dae9ff9f582',1,'manifold::Manifold']]],
+  ['the_20author_2',['About the author',['../index.html#autotoc_md16',1,'']]],
+  ['timer_3',['Timer',['../structmanifold_1_1_timer.html',1,'manifold']]],
+  ['tolerance_4',['tolerance',['../structmanifold_1_1_mesh_g_l_p.html#ab47a93bffcbb73ec3769de2cc6c695f2',1,'manifold::MeshGLP']]],
+  ['topologyerr_5',['topologyErr',['../structtopology_err.html',1,'']]],
+  ['topolygons_6',['ToPolygons',['../classmanifold_1_1_cross_section.html#a209a83b1c39a84e094da9e854a7b8bc2',1,'manifold::CrossSection']]],
+  ['torusknot_7',['TorusKnot',['../group___samples.html#ga0ccde255ceda709d8e4a54ec95f3ca3e',1,'manifold']]],
+  ['transform_8',['Transform',['../classmanifold_1_1_manifold.html#a69976677a7a332a1ad69c3fcf216e2bf',1,'manifold::Manifold::Transform()'],['../classmanifold_1_1_cross_section.html#a4c84b6c8b521f867f7fa7416d36909de',1,'manifold::CrossSection::Transform()'],['../structmanifold_1_1_box.html#a268eaff1405d22443331cd697a46ad54',1,'manifold::Box::Transform()'],['../structmanifold_1_1_rect.html#a9e0b6af248c25d0154dbf0d0b1cddb1c',1,'manifold::Rect::Transform()']]],
+  ['transforms_9',['Transforms',['../group__transforms.html',1,'']]],
+  ['translate_10',['Translate',['../classmanifold_1_1_manifold.html#a78fbea69555ea755f8731255060f7c9d',1,'manifold::Manifold::Translate()'],['../classmanifold_1_1_cross_section.html#a38bcb89b28140acbceca56f0bbda67b9',1,'manifold::CrossSection::Translate()']]],
+  ['triangulate_11',['Triangulate',['../group___triangulation.html#gad94a4b0c8fcbba40756f72d741adf160',1,'manifold']]],
+  ['triangulateidx_12',['TriangulateIdx',['../group___triangulation.html#gacb67114040ef7ce7a762bf52285b77a9',1,'manifold']]],
+  ['triangulation_13',['Triangulation',['../group___triangulation.html',1,'']]],
+  ['trimbyplane_14',['TrimByPlane',['../classmanifold_1_1_manifold.html#aa8d62161da6e94da7651418c33c3e294',1,'manifold::Manifold']]],
+  ['triverts_15',['triVerts',['../structmanifold_1_1_mesh_g_l_p.html#ab0aa7d9b8f8adbb059ff0c6b70d38e6e',1,'manifold::MeshGLP']]]
+];
---
+++ docs/html/search/functions_9.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['manifold_0',['Manifold',['../classmanifold_1_1_manifold.html#ad5f60691d05e3402630122740cdf3a0b',1,'manifold::Manifold::Manifold()'],['../classmanifold_1_1_manifold.html#a7bbdc9ecbc563edb1fa67828c747b0ae',1,'manifold::Manifold::Manifold(const MeshGL &amp;)'],['../classmanifold_1_1_manifold.html#a5295e63030e90f0e138d8548ad4e87b5',1,'manifold::Manifold::Manifold(const MeshGL64 &amp;)']]],
+  ['manifoldparams_1',['ManifoldParams',['../group___debug.html#ga4a00dd8e8dff2c4f8a9105d1a5558275',1,'manifold']]],
+  ['matchestrinormals_2',['MatchesTriNormals',['../classmanifold_1_1_manifold.html#a611a5fd95dc5d49cd88182b8ead722b7',1,'manifold::Manifold']]],
+  ['mengersponge_3',['MengerSponge',['../group___samples.html#ga31bc13b03579ca41da7ae9b5ba171fae',1,'manifold']]],
+  ['merge_4',['Merge',['../structmanifold_1_1_mesh_g_l_p.html#a2078d075bef279041b67769e4197c254',1,'manifold::MeshGLP']]],
+  ['mingap_5',['MinGap',['../classmanifold_1_1_manifold.html#a1b7ab47f4da039eb3dff5b46ba71658b',1,'manifold::Manifold']]],
+  ['mirror_6',['Mirror',['../classmanifold_1_1_manifold.html#ad76d89178ca970900c81f2927f8c7f11',1,'manifold::Manifold::Mirror()'],['../classmanifold_1_1_cross_section.html#a3bdcbaf979a769af24a9d61624e639e8',1,'manifold::CrossSection::Mirror()']]]
+];
