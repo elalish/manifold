605e05e1dbdfba01129d18ecbc5b0fdff01d89df
elalish <elalish@users.noreply.github.com>
2025-06-12 07:43:47 +0000

    deploy: 87b8167659ed375ae41ed41c39f74f8fa6b1c8fa

---
+++ docs/html/search/all_12.js
@@ -0,0 +1,27 @@
+var searchData=
+[
+  ['radians_0',['radians',['../group___scalar.html#gad3c065a3fc8ae345aaa374879017854a',1,'manifold']]],
+  ['rect_1',['Rect',['../structmanifold_1_1_rect.html',1,'Rect'],['../structmanifold_1_1_rect.html#abc2e4ec957e1fee58d58bd51c0a68191',1,'manifold::Rect::Rect()'],['../structmanifold_1_1_rect.html#a46aa10a4a70d0d3027d4a4bdde3322cd',1,'manifold::Rect::Rect(const vec2 a, const vec2 b)']]],
+  ['reductions_2',['Reductions',['../group__reductions.html',1,'']]],
+  ['refine_3',['Refine',['../classmanifold_1_1_manifold.html#a5d9227a2d10d8ad7b005555213ecd1ac',1,'manifold::Manifold']]],
+  ['refinetolength_4',['RefineToLength',['../classmanifold_1_1_manifold.html#a1381728e52975862d954032f717106d5',1,'manifold::Manifold']]],
+  ['refinetotolerance_5',['RefineToTolerance',['../classmanifold_1_1_manifold.html#a723efa99b25f74d4099f004a47313be1',1,'manifold::Manifold']]],
+  ['relational_6',['Relational',['../group__relational.html',1,'']]],
+  ['research_20google_20com_20drive_201vxrfyhpshzgubl9tewzceovlpxrpq5j5_20usp_20sharing_20python_20colab_20example_20a_7',['&lt;a href=&quot;https://colab.research.google.com/drive/1VxrFYHPSHZgUbl9TeWzCeovlpXrPQ5J5?usp=sharing&quot; &gt;Python Colab Example&lt;/a&gt;',['../index.html#autotoc_md5',1,'']]],
+  ['reserveids_8',['ReserveIDs',['../classmanifold_1_1_manifold.html#a1d7e7e674ce7bd9954dc0d54ec6ebc8c',1,'manifold::Manifold']]],
+  ['resettodefaults_9',['ResetToDefaults',['../classmanifold_1_1_quality.html#a4595203dc1f079e03b20e9d77e868535',1,'manifold::Quality']]],
+  ['revolve_10',['Revolve',['../classmanifold_1_1_manifold.html#a8c454a293abbf86f520e10266087f5a6',1,'manifold::Manifold']]],
+  ['rot_11',['rot',['../group__vec__algebra.html#ga15bcc45a6ddb10fe7ff60aaebcea304d',1,'linalg']]],
+  ['rotate_12',['Rotate',['../classmanifold_1_1_manifold.html#a7f74bf3b7c5dd34e84b82e7738fd25ff',1,'manifold::Manifold::Rotate()'],['../classmanifold_1_1_cross_section.html#a794f36c935736ecf71806fb3ccd46956',1,'manifold::CrossSection::Rotate()']]],
+  ['rotation_5fquat_13',['rotation_quat',['../group__quaternion__rotation.html#gaa6292de23f466f4e0c0da188378947d1',1,'linalg::rotation_quat(const vec&lt; T, 3 &gt; &amp;axis, T angle)'],['../group__quaternion__rotation.html#ga8111957451021af7c66d8d37e5aea3c9',1,'linalg::rotation_quat(const vec&lt; T, 3 &gt; &amp;orig, const vec&lt; T, 3 &gt; &amp;dest)'],['../group__quaternion__rotation.html#ga125d7bb18e01e1f7247cb2b90886cb96',1,'linalg::rotation_quat(const mat&lt; T, 3, 3 &gt; &amp;m)']]],
+  ['rotations_14',['Quaternion Rotations',['../group__quaternion__rotation.html',1,'']]],
+  ['rotx_15',['rotx',['../group__vec__algebra.html#ga294ebb36141ba696800b518cb99be038',1,'linalg']]],
+  ['roty_16',['roty',['../group__vec__algebra.html#ga0a3590f1a5b4fcae70db8f43da823ce8',1,'linalg']]],
+  ['rotz_17',['rotz',['../group__vec__algebra.html#gaee21f91e6491b605605877fb9c292f43',1,'linalg']]],
+  ['roughness_18',['roughness',['../group___mesh_i_o.html#aae6a29bf1aea8336238154d7b4b95994',1,'manifold::Material']]],
+  ['round_19',['Round',['../classmanifold_1_1_cross_section.html#aef400c43b34e3ecc3f7b342aa821395dab7f41fc1412ad2ee75e9b2635d3b9d5c',1,'manifold::CrossSection']]],
+  ['roundedframe_20',['RoundedFrame',['../group___samples.html#gaed7341c943d9bb53a52f01f4d9453bd8',1,'manifold']]],
+  ['runindex_21',['runIndex',['../structmanifold_1_1_mesh_g_l_p.html#a3d13347a121827c135c328194009b264',1,'manifold::MeshGLP']]],
+  ['runoriginalid_22',['runOriginalID',['../structmanifold_1_1_mesh_g_l_p.html#aa67d01a9b240154a754945eca9fca3e0',1,'manifold::MeshGLP']]],
+  ['runtransform_23',['runTransform',['../structmanifold_1_1_mesh_g_l_p.html#a4a2f4e21de0e8026daf67353dfd3458b',1,'manifold::MeshGLP']]]
+];
