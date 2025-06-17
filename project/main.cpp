27aa98caa907472be92810963516c9f25e32254c
Trzeth <Trzeth@outlook.com>
2025-06-18 01:11:25 +0800

    merging from python

--- project/main.cpp
+++ project/main.cpp
@@ -12,12 +12,8 @@ using namespace manifold;
 vec3 toVec3(vec2 in) { return vec3(in.x, in.y, 0); }
 
 void VertexByVertex(double radius) {
-  manifold::Polygons poly{{vec2{0, 0}, vec2{1, 1}, vec2{0, 1}}};
-
-  for (size_t i = 0; i != poly[0].size(); i++) {
-    const vec2 preP = poly[0][(i - 1 + poly[0].size()) % poly[0].size()],
-               curP = poly[0][i], nextP = poly[0][(i + 1) % poly[0].size()];
-
+  auto generateArc = [](const vec2& preP, const vec2& curP, const vec2& nextP,
+                        double radius) -> std::vector<vec2> {
     vec2 norm1 = la::normalize(preP - curP),
          norm2 = la::normalize(nextP - curP);
     double theta = std::acos(la::dot(norm1, norm2));
@@ -34,6 +30,52 @@ void VertexByVertex(double radius) {
            end = std::atan2(t2.y - circleCenter.y, t2.x - circleCenter.x);
 
     // Discrete, Merge
+
+    double sweep_angle_total_rad = end - start;
+
+    const double EPSILON = 1E-7;
+
+    if (convexity > 0 && sweep_angle_total_rad < EPSILON) {
+      sweep_angle_total_rad += 2 * M_PI;
+    } else if (convexity < 0 && sweep_angle_total_rad > EPSILON) {
+      sweep_angle_total_rad -= 2 * M_PI;
+    }
+
+    int num_arc_segments = 100;
+
+    std::vector<vec2> arcPoints;
+    arcPoints.push_back(t1);
+
+    for (int i = 1; i < num_arc_segments; ++i) {
+      double fraction = static_cast<double>(i) / num_arc_segments;
+      double current_angle = start + sweep_angle_total_rad * fraction;
+      double px = circleCenter.x + radius * std::cos(current_angle);
+      double py = circleCenter.y + radius * std::sin(current_angle);
+      arcPoints.push_back({px, py});
+    }
+
+    arcPoints.push_back(t2);
+
+    return arcPoints;
+  };
+
+  manifold::Polygons poly{{vec2{0, 0}, vec2{1, 1}, vec2{0, 1}}};
+  manifold::Polygons newPoly{{}};
+
+  for (size_t i = 0; i != poly[0].size(); i++) {
+    auto& loop = poly[0];
+    auto& newLoop = newPoly[0];
+
+    const vec2 preP = loop[(i - 1 + loop.size()) % loop.size()], curP = loop[i],
+               nextP = loop[(i + 1) % loop.size()];
+
+    auto r = generateArc(preP, curP, nextP, radius);
+
+    if (!r.empty()) {
+      newLoop.insert(newLoop.end(), r.begin(), r.end());
+    } else {
+      newLoop.push_back(curP);
+    }
   }
 }
 
@@ -56,7 +98,7 @@ void RollingBall(double radius) {
     boxVec.push_back(bbox);
   }
 
-  // manifold::Collider collider(boxVec, mortonVec);
+  manifold::Collider collider(boxVec, mortonVec);
 
   for (size_t i = 0; i != poly[0].size(); i++) {
     const vec2 p1 = poly[0][i], p2 = poly[0][(i + 1) % poly[0].size()];
@@ -74,8 +116,7 @@ void RollingBall(double radius) {
     box.Union(toVec3(offsetP1));
     box.Union(toVec3(offsetP2));
 
-    // auto r =
-    // collider.Collisions(manifold::Vec<manifold::Box>({box}).cview());
+    auto r = collider.Collisions(manifold::Vec<manifold::Box>({box}).cview());
   }
 }
 
