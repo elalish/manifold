9ed6062f560016aaebb9701739e021f836026b6e
elalish <elalish@users.noreply.github.com>
2025-07-19 03:59:59 +0000

    deploy: dad5deeccc1cd2808f0103f7811b0b4434598b4e

---
+++ docs/html/search/enums_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['jointype_0',['JoinType',['../classmanifold_1_1_cross_section.html#aef400c43b34e3ecc3f7b342aa821395d',1,'manifold::CrossSection']]]
+];
---
+++ docs/html/search/typedefs_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['meshgl_0',['MeshGL',['../group___core.html#ga4c4992743d3cf12ecd9f51b570a77a5f',1,'manifold']]],
+  ['meshgl64_1',['MeshGL64',['../group___core.html#ga061363496a3324e9b95ee66eab1c88d6',1,'manifold']]]
+];
