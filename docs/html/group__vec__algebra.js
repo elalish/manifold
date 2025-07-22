2108733d37db83a0587383528a3b65ea6fdec68f
elalish <elalish@users.noreply.github.com>
2025-07-22 20:59:06 +0000

    deploy: f1962a723cb789e018292f842165c4ccc8c0e7fe

---
+++ docs/html/group__vec__algebra.js
@@ -0,0 +1,21 @@
+var group__vec__algebra =
+[
+    [ "cross", "group__vec__algebra.html#gae5dc172bd9041ee4ba8ad2ecc9b3772b", null ],
+    [ "cross", "group__vec__algebra.html#gac951d66e3e8e6bf8e9deab2d3eece4e2", null ],
+    [ "cross", "group__vec__algebra.html#ga75fb29ccd38c1fd5d078ffb4a22a2ab4", null ],
+    [ "cross", "group__vec__algebra.html#ga9c2699c225b113fa8367a59d8cecb5d2", null ],
+    [ "dot", "group__vec__algebra.html#ga7ac5d3ed262611340e95129e540eaf0a", null ],
+    [ "length2", "group__vec__algebra.html#ga62fc2092370a3257fb759ee17ec7c23f", null ],
+    [ "length", "group__vec__algebra.html#gaf95c4ba484d6efe575e8fe8225f7203f", null ],
+    [ "normalize", "group__vec__algebra.html#ga831cd65b7277c44a791a507e77ea2f0a", null ],
+    [ "distance2", "group__vec__algebra.html#ga27cbd479fb849af7f57ada1e551de768", null ],
+    [ "distance", "group__vec__algebra.html#gafa2e270a2bd944eef65542243670f65f", null ],
+    [ "uangle", "group__vec__algebra.html#gab0d86accbda99f38bf3c60f03b03504a", null ],
+    [ "angle", "group__vec__algebra.html#ga91b164bccfb3fde27e8a2bb7ee378afb", null ],
+    [ "rot", "group__vec__algebra.html#ga15bcc45a6ddb10fe7ff60aaebcea304d", null ],
+    [ "rotx", "group__vec__algebra.html#ga294ebb36141ba696800b518cb99be038", null ],
+    [ "roty", "group__vec__algebra.html#ga0a3590f1a5b4fcae70db8f43da823ce8", null ],
+    [ "rotz", "group__vec__algebra.html#gaee21f91e6491b605605877fb9c292f43", null ],
+    [ "nlerp", "group__vec__algebra.html#ga80d7b49f19a1d93752a5479cd07a428a", null ],
+    [ "slerp", "group__vec__algebra.html#ga865d3c559380616dc94880a819dc295f", null ]
+];
