503590e93936a8e0b2911e7b11156a7768152d1a
pca006132 <pca006132@users.noreply.github.com>
2025-08-08 12:08:53 +0000

    deploy: ae57329b6788511d34d3a1862caf3dd01f6cb8eb

---
+++ docs/html/search/functions_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['angle_0',['angle',['../group__vec__algebra.html#ga91b164bccfb3fde27e8a2bb7ee378afb',1,'linalg']]],
+  ['area_1',['Area',['../classmanifold_1_1_cross_section.html#ac84b151a969c68d662637e8245548666',1,'manifold::CrossSection::Area()'],['../structmanifold_1_1_rect.html#a755a612bb3ecc4b74662828545e0e442',1,'manifold::Rect::Area()']]],
+  ['asoriginal_2',['AsOriginal',['../classmanifold_1_1_manifold.html#a9b4bbbc5b70a0d1f6b07c91f1684fb57',1,'manifold::Manifold']]]
+];
