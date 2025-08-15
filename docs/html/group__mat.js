74f11c5c586a29192697efe1e01826873846fc04
pca006132 <pca006132@users.noreply.github.com>
2025-08-15 16:15:56 +0000

    deploy: e047ba6362d199bef40030db4ee19cd5f387f258

---
+++ docs/html/group__mat.js
@@ -0,0 +1,7 @@
+var group__mat =
+[
+    [ "mat< T, M, 1 >", "structlinalg_1_1mat_3_01_t_00_01_m_00_011_01_4.html", null ],
+    [ "mat< T, M, 2 >", "structlinalg_1_1mat_3_01_t_00_01_m_00_012_01_4.html", null ],
+    [ "mat< T, M, 3 >", "structlinalg_1_1mat_3_01_t_00_01_m_00_013_01_4.html", null ],
+    [ "mat< T, M, 4 >", "structlinalg_1_1mat_3_01_t_00_01_m_00_014_01_4.html", null ]
+];
---
+++ docs/html/search/variables_a.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['tolerance_0',['tolerance',['../structmanifold_1_1_mesh_g_l_p.html#ab47a93bffcbb73ec3769de2cc6c695f2',1,'manifold::MeshGLP']]],
+  ['triverts_1',['triVerts',['../structmanifold_1_1_mesh_g_l_p.html#ab0aa7d9b8f8adbb059ff0c6b70d38e6e',1,'manifold::MeshGLP']]]
+];
