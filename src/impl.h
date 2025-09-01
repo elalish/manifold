264e49a359292befeb54667eb80d6688d62701ed
Trzeth <Trzeth@outlook.com>
2025-09-01 14:27:43 +0800

    update

--- src/impl.h
+++ src/impl.h
@@ -68,7 +68,12 @@ struct Manifold::Impl {
     const uint32_t numVert = meshGL.NumVert();
     const uint32_t numTri = meshGL.NumTri();
 
-    if ((numVert && numVert < 4) || (numTri && numTri < 4)) {
+    if (!numVert && !numTri) {
+      MakeEmpty(Error::NoError);
+      return;
+    }
+
+    if ((numVert < 4) || (numTri < 4)) {
       MakeEmpty(Error::NotManifold);
       return;
     }
