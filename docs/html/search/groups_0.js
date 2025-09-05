4cde5c97ce1bfeb48c6fc5dc7b2ad88127d3e4af
elalish <elalish@users.noreply.github.com>
2025-09-05 08:52:57 +0000

    deploy: 557b320729814b54eb70f1ceb6028721a529c28c

---
+++ docs/html/search/groups_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['algebra_0',['Vector Algebra',['../group__vec__algebra.html',1,'']]],
+  ['apply_1',['Apply',['../group__apply.html',1,'']]],
+  ['array_2',['Array',['../group__array.html',1,'']]]
+];
---
+++ docs/html/search/variables_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['cleanuptriangles_0',['cleanupTriangles',['../group___debug.html#a85c1664e476e1da1b3d0973b3451857b',1,'manifold::ExecutionParams']]],
+  ['color_1',['color',['../group___mesh_i_o.html#a25f2e1fef7cecde0255a3dde64ab4979',1,'manifold::Material']]],
+  ['coloridx_2',['colorIdx',['../group___mesh_i_o.html#ad73afde22525668518f86204dacd4491',1,'manifold::Material']]]
+];
