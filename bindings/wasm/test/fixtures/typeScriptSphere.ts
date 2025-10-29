ec66783b7a25cc706c3a7a05d80d686a8c01896d
Tony Thompson <tony@thompson.name>
2025-10-29 11:20:22 -0400

    It has been [0] days since the last linter error.

--- bindings/wasm/test/fixtures/typeScriptSphere.ts
+++ bindings/wasm/test/fixtures/typeScriptSphere.ts
@@ -1,8 +1,8 @@
 import {Manifold} from '../../lib/manifoldCAD';
 
-const makeSphere: ((radius?:number) => any) = (radius?:number) => {
-  return Manifold.sphere(radius ?? 1.0)
-}
+const makeSphere: ((radius?: number) => any) = (radius?: number) => {
+  return Manifold.sphere(radius ?? 1.0);
+};
 
 const result = makeSphere();
 export default result;
