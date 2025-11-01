7025aed2fa9c731d4bc2e3f561d7745701ede7ac
elalish <elalish@users.noreply.github.com>
2025-11-01 08:34:42 +0000

    deploy: e93f95dc046e641f512bf3864f312000e03416d1

---
+++ docs/html/group___mesh_i_o_structmanifold_1_1_material.js
@@ -0,0 +1,10 @@
+var group___mesh_i_o_structmanifold_1_1_material =
+[
+    [ "roughness", "group___mesh_i_o.html#aae6a29bf1aea8336238154d7b4b95994", null ],
+    [ "metalness", "group___mesh_i_o.html#a75517e1226a2805e9bc0b78668b01812", null ],
+    [ "color", "group___mesh_i_o.html#a25f2e1fef7cecde0255a3dde64ab4979", null ],
+    [ "alpha", "group___mesh_i_o.html#a20f4c4490bc8ecbdd1ffcb79acce6035", null ],
+    [ "normalIdx", "group___mesh_i_o.html#a8bf2a091ffda73fc32c5bf237e00870a", null ],
+    [ "colorIdx", "group___mesh_i_o.html#ad73afde22525668518f86204dacd4491", null ],
+    [ "alphaIdx", "group___mesh_i_o.html#a252d015e56b5a667a431b084bba2e9e7", null ]
+];
---
+++ docs/html/group__array.js
@@ -0,0 +1,11 @@
+var group__array =
+[
+    [ "converter< vec< T, 1 >, std::array< T, 1 > >", "structlinalg_1_1converter_3_01vec_3_01_t_00_011_01_4_00_01std_1_1array_3_01_t_00_011_01_4_01_4.html", null ],
+    [ "converter< vec< T, 2 >, std::array< T, 2 > >", "structlinalg_1_1converter_3_01vec_3_01_t_00_012_01_4_00_01std_1_1array_3_01_t_00_012_01_4_01_4.html", null ],
+    [ "converter< vec< T, 3 >, std::array< T, 3 > >", "structlinalg_1_1converter_3_01vec_3_01_t_00_013_01_4_00_01std_1_1array_3_01_t_00_013_01_4_01_4.html", null ],
+    [ "converter< vec< T, 4 >, std::array< T, 4 > >", "structlinalg_1_1converter_3_01vec_3_01_t_00_014_01_4_00_01std_1_1array_3_01_t_00_014_01_4_01_4.html", null ],
+    [ "converter< std::array< T, 1 >, vec< T, 1 > >", "structlinalg_1_1converter_3_01std_1_1array_3_01_t_00_011_01_4_00_01vec_3_01_t_00_011_01_4_01_4.html", null ],
+    [ "converter< std::array< T, 2 >, vec< T, 2 > >", "structlinalg_1_1converter_3_01std_1_1array_3_01_t_00_012_01_4_00_01vec_3_01_t_00_012_01_4_01_4.html", null ],
+    [ "converter< std::array< T, 3 >, vec< T, 3 > >", "structlinalg_1_1converter_3_01std_1_1array_3_01_t_00_013_01_4_00_01vec_3_01_t_00_013_01_4_01_4.html", null ],
+    [ "converter< std::array< T, 4 >, vec< T, 4 > >", "structlinalg_1_1converter_3_01std_1_1array_3_01_t_00_014_01_4_00_01vec_3_01_t_00_014_01_4_01_4.html", null ]
+];
