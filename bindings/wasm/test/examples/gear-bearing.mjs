c82ff60ff55ac3aca983bfd4722a9ea9f60c3bcb
Emmett Lalish <elalish@gmail.com>
2025-11-12 22:01:53 +1300

    updated involute gear library and exmaple

---
+++ bindings/wasm/test/examples/gear-bearing.mjs
@@ -0,0 +1,95 @@
+// Demonstrate using a library.
+// This example recreates the Gear Bearing by Emmett Lalish:
+// https://www.thingiverse.com/thing:53451
+
+import {getGLTFNodes, GLTFNode, Manifold} from 'manifold-3d/manifoldCAD';
+
+import {spurGear} from './involute-gear-library';
+
+const {round, min, cos, tan, sqrt, PI} = Math;
+
+const diameter = 51.7;
+const thickness = 15;
+const clearance = 0.15;
+const planets = 5;
+const planetTeeth = 7;
+const sunTeeth = 9;
+const pressureDeg = 45;
+const holeWidth = 6.7;
+setMinCircularAngle(2);
+setMinCircularEdgeLength(0.5);
+
+const depthRatio = 0.5;
+const m = round(planets);
+const np = round(planetTeeth);
+const k1 = round(2 / m * (sunTeeth + np));
+const k = k1 * m % 2 != 0 ? k1 + 1 : k1;
+const ns = k * m / 2 - np;
+const nr = ns + 2 * np;
+const pitchD = 0.9 * diameter /
+    (1 +
+     min(PI / (2 * nr * tan(pressureDeg * PI / 180)), PI * depthRatio / nr));
+const circularPitch = pitchD * PI / nr;
+const twist = circularPitch * 2 / thickness;
+const holeRadius = holeWidth / sqrt(3);
+
+const gearParams = {
+  circularPitch,
+  pressureDeg,
+  clearance,
+  depthRatio,
+  twist
+};
+
+function chevronGear(teeth, clearanceFactor = 1) {
+  const params = {...gearParams};
+  params.clearance *= clearanceFactor;
+  const top = spurGear(teeth, thickness / 2, params);
+  return top.add(top.mirror([0, 0, 1]));
+}
+
+const sun = chevronGear(ns).mirror([0, 1, 0]).subtract(
+    Manifold.cylinder(thickness + 1, holeRadius, holeRadius, 6, true));
+const ring = Manifold.cylinder(thickness, diameter / 2, diameter / 2, 0, true)
+                 .subtract(chevronGear(nr, -1));
+const planet = chevronGear(np);
+
+const shift = new GLTFNode();
+shift.translation = [0, 0, thickness / 2];
+
+const ringNode = new GLTFNode(shift);
+ringNode.manifold = ring;
+ringNode.material = {
+  baseColorFactor: [0.3, 0.3, 0.3]
+};
+
+const sunNode = new GLTFNode(shift);
+sunNode.manifold = sun;
+sunNode.rotation =
+    (t) => [0, 0, (np + 1) * 180 * m / ns + (t * 360) * (ns + np) * 2 / ns]
+sunNode.material = {
+  baseColorFactor: [0.7, 0.7, 0.7]
+};
+
+for (let i = 0; i < planets; ++i) {
+  const planetCarrier = new GLTFNode(shift);
+  planetCarrier.rotation = (t) => [0, 0, (i / m + t) * 360];
+
+  const planetNode = new GLTFNode(planetCarrier);
+  planetNode.manifold = planet;
+  planetNode.rotation =
+      (t) => [0, 0, i * ns / m * 360 / np - (t * 360) * (1 + (ns + np) / np)];
+  planetNode.translation = [pitchD / 2 * (ns + np) / nr, 0, 0];
+  planetNode.material = {
+    baseColorFactor: [
+      0.3 * cos(2 * PI * i / m) + 0.5,
+      0.2 * cos(2 * PI * (i / m + 1 / 3)) + 0.5,
+      0.8 * cos(2 * PI * (i / m + 2 / 3)) + 0.5
+    ]
+  };
+}
+
+setAnimationDuration(5);  // GLTF animation length in seconds
+
+const bearing = getGLTFNodes();
+export default bearing;
--- bindings/wasm/test/examples/involute-gear-library.mjs
+++ bindings/wasm/test/examples/involute-gear-library.mjs
@@ -1,82 +1,124 @@
 // Demonstrate how to build a library for manifoldCAD
 //
-// Create a CrossSection of an involute gear.
-// Based on the openJScad example written by Joost Nieuwenhuijse and Simon Clark
-// https://github.com/jscad/OpenJSCAD.org/blob/master/packages/examples/parameters/gear.js
+// This example is based on the involute gear modules from the Gear Bearing by
+// Emmett Lalish: https://www.thingiverse.com/thing:53451
 
-import {CrossSection} from "manifold-3d/manifoldCAD";
-const {circle, hull, union} = CrossSection;
-const {PI, atan, cos, sin, sqrt} = Math;
-const TAU = PI*2;
+import {CrossSection, getCircularSegments, Vec2} from 'manifold-3d/manifoldCAD';
 
-const involute = (r, psi) => ([
-  r*(cos(psi)+psi*sin(psi)),
-  r*(sin(psi)-psi*cos(psi))
-]);
+const {sin, cos, tan, atan2, sqrt, pow, max, PI} = Math;
 
-const gearParams = (args = {}) => {
-  const opts = {
-    module: 1,
-    teeth: 12,
-    pressureAngle: 20,
-    addendum: 1,
-    dedendum: 1.25,
-    ...args
-  };
+/**
+ * Create a 3D spur or helical gear.
+ * @param {number} teeth - number of teeth
+ * @param {number} height - height/thickness of the gear
+ * @param {object} params - gear parameters: should match for meshing gears
+ * @param {number} params.circularPitch - tooth spacing - default 10mm
+ * @param {number} params.pressureDeg - contact angle - default 20 degrees
+ * @param {number} params.depthRatio - tooth length / tooth spacing - default
+ *     0.75
+ * @param {number} params.clearance - gap between meshing teeth - default 0mm
+ * @param {number} params.n - number of points per curve - default is
+ * calculated by the static Quality defaults according to the circularPitch
+ * @param {number} params.twist - helical pitch twist / height - default 0
+ * @returns {Manifold} - the spur gear manifold
+ */
+export function spurGear(teeth: number, height: number, {
+  circularPitch = 10,
+  pressureDeg = 20,
+  depthRatio = 0.75,
+  clearance = 0,
+  n = getCircularSegments(circularPitch / 6),
+  twist = 0
+} = {}) {
+  const nDiv = n * twist * height / circularPitch;
+  const twistDeg = twist * height * 360 / (teeth * circularPitch);
+  return gear2D(teeth, {circularPitch, pressureDeg, depthRatio, clearance, n})
+      .extrude(height, nDiv, twistDeg);
+}
 
-  const { pressureAngle, module, teeth, addendum, dedendum } = opts;
-  const pitchRadius = module * teeth / 2;
-  const tipRadius = pitchRadius + addendum * module;
-  const rootRadius = pitchRadius - dedendum * module;
-  const baseRadius = pitchRadius * cos(pressureAngle * TAU/360);
+/* Create a 2D CrossSection of an involute gear.
+ * @param {number} teeth - number of teeth
+ * @param {object} params - gear parameters: should match for meshing gears
+ * @param {number} params.circularPitch - tooth spacing (mm)
+ * @param {number} params.pressureDeg - contact angle
+ * @param {number} params.depthRatio - tooth length / tooth spacing
+ * @param {number} params.clearance - gap between meshing teeth
+ * @param {number} params.n - number of points per curve
+ * @returns {CrossSection} - the spur gear cross section
+ */
+export function gear2D(teeth: number, {
+  circularPitch = 10,
+  pressureDeg = 20,
+  depthRatio = 0.75,
+  clearance = 0,
+  n = getCircularSegments(circularPitch / 6),
+} = {}) {
+  const pressureRad = pressureDeg * PI / 180;
+  const pitchRadius = teeth * circularPitch / (2 * PI);
+  const baseRadius = pitchRadius * cos(pressureRad);
+  const depth = circularPitch / (2 * tan(pressureRad));
+  const outerRadius = clearance < 0 ? pitchRadius + depth / 2 - clearance :
+                                      pitchRadius + depth / 2;
+  const rootRadius1 = pitchRadius - depth / 2 - clearance / 2;
+  const rootRadius =
+      (clearance < 0 && rootRadius1 < baseRadius) ? baseRadius : rootRadius1;
 
-  return {
-    ...opts,
-    pitchRadius, tipRadius, rootRadius, baseRadius,
-  };
-};
+  const backlashDeg = clearance / (pitchRadius * cos(pressureRad)) * 180 / PI;
+  const halfThickDeg = 90 / teeth - backlashDeg / 2;
 
-const tooth2d = (params) => {
-  const {baseRadius, pitchRadius, tipRadius, teeth} = params;
-  const maxTanLength = sqrt(tipRadius**2 - baseRadius**2);
-  const maxAngle = maxTanLength / baseRadius;
+  const pitchPoint =
+      involute(baseRadius, involuteIntersectRad(baseRadius, pitchRadius));
+  const pitchDeg = atan2(pitchPoint[1], pitchPoint[0]) * 180 / PI;
+  const minRadius = max(baseRadius, rootRadius);
 
-  const n = 8;
-  const points = [[0,0]];
-  for(let i=0; i<=n; i++) {
-    const angle = i*maxAngle/n;
-    points.push(involute(baseRadius,angle))
+  const startRad = max(involuteIntersectRad(baseRadius, minRadius) - n, 0);
+  const stopRad = involuteIntersectRad(baseRadius, outerRadius);
+  const step = (stopRad - startRad) / n;
+
+  const p = [[0, 0] as Vec2];
+  for (let i = 0; i <= n; ++i) {
+    p.push(involute(baseRadius, i * step + startRad));
   }
 
-  const toothWidthAtPitchCircle = sqrt(pitchRadius**2-baseRadius**2);
-  const angleAtPitchCircle = toothWidthAtPitchCircle / baseRadius;
-  const diffAngle = angleAtPitchCircle - atan(angleAtPitchCircle);
-  const angularToothWidthAtBase = (TAU/teeth) + diffAngle;
+  const halftooth = new CrossSection(p)
+                        .rotate(-pitchDeg - halfThickDeg)
+                        .subtract(CrossSection.square(2 * outerRadius));
+  const tooth = halftooth.add(halftooth.mirror([0, 1]));
 
-  const halfTooth = hull(points)
-    .rotate(-2*angularToothWidthAtBase)
-    .rotate(-360/teeth * 1/4);
-  return hull([halfTooth,halfTooth.mirror([0,1])]);
-};
+  let gear =
+      CrossSection
+          .circle(
+              max(rootRadius,
+                  pitchRadius - depthRatio * circularPitch / 2 - clearance / 2),
+              teeth * 2)
+          .rotate(90 / teeth);
+  for (let i = 0; i < teeth; ++i) {
+    gear = gear.add(tooth.rotate(i * 360 / teeth));
+  }
+  return gear.intersect(
+      CrossSection
+          .circle(
+              pitchRadius + depthRatio * circularPitch / 2 - clearance / 2,
+              teeth * 3)
+          .rotate(90 / teeth));
+}
 
-// Export this function.  ManifoldCAD will not call it directly,
-// but other models can call it.  See 'Animated Gears' for an example.
-export const involuteGear2d = (args = {}) => {
-  const params = gearParams(args);
-  const {teeth, rootRadius} = params;
+// Calculate the involute angle at a given radius larger than the base radius.
+function involuteIntersectRad(baseRadius, radius) {
+  return sqrt(pow(radius / baseRadius, 2) - 1);
+}
 
-  const t = tooth2d(params);
-  return union(
-    [...Array(teeth).keys()]
-      .map(n => t.rotate(n * 360/teeth))
-  ).add(
-    circle(rootRadius)
-  );
-};
+// Calculate the involute position for a given base radius and involute angle.
+function involute(baseRadius, involuteRad): Vec2 {
+  return [
+    baseRadius * (cos(involuteRad) + involuteRad * sin(involuteRad)),
+    baseRadius * (sin(involuteRad) - involuteRad * cos(involuteRad))
+  ]
+}
 
 // ManifoldCAD will attempt to render to the default export of a model.
 // This can be a Manifold object or a function that returns a Manifold object.
 // It will not run when importing this file, unless explicitly called.
 export default () => {
-  return involuteGear2d({teeth: 12}).extrude(1.5);
+  return spurGear(15, 5, {circularPitch: 5});
 };
