3a0d9b21ecf843dc98816a2f250679797903bd7e
pca006132 <pca006132@users.noreply.github.com>
2025-12-27 08:54:45 +0000

    deploy: b2c6ddd2e33510498a62c89049c83fa70342ddb3

---
+++ docs/html/group___debug.js
@@ -0,0 +1,15 @@
+var group___debug =
+[
+    [ "ExecutionParams", "group___debug.html#structmanifold_1_1_execution_params", [
+      [ "intermediateChecks", "group___debug.html#ae8dc7c8ba98011f6f2dbe1caddf2a6fb", null ],
+      [ "selfIntersectionChecks", "group___debug.html#aca3506c8f5774ba33cd774fd54b20d4a", null ],
+      [ "processOverlaps", "group___debug.html#a7166fd3caef3744f7241075f21f76af3", null ],
+      [ "suppressErrors", "group___debug.html#a48b673772893c29c426e98f8c84f0206", null ],
+      [ "cleanupTriangles", "group___debug.html#a85c1664e476e1da1b3d0973b3451857b", null ],
+      [ "verbose", "group___debug.html#a0b2caeb4b6f130be43e5a2f0267dd453", null ]
+    ] ],
+    [ "userErr", "structuser_err.html", null ],
+    [ "topologyErr", "structtopology_err.html", null ],
+    [ "geometryErr", "structgeometry_err.html", null ],
+    [ "ManifoldParams", "group___debug.html#ga4a00dd8e8dff2c4f8a9105d1a5558275", null ]
+];
---
+++ docs/html/search/enumvalues_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['evenodd_0',['EvenOdd',['../classmanifold_1_1_cross_section.html#a9a534b0377c9ca41983d53b0dae0d5a4a8e586e4bd2c45e86222301f2e6e0e390',1,'manifold::CrossSection']]]
+];
