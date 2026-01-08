3c5389e802b42d3c7b9c3f82687160c7f53902aa
elalish <elalish@users.noreply.github.com>
2026-01-08 18:53:10 +0000

    deploy: 8732952b5fd22a0334b4ec02307c75d3170089ed

---
+++ docs/html/group__hash.js
@@ -0,0 +1,11 @@
+var group__hash =
+[
+    [ "hash< linalg::vec< T, 1 > >", "structstd_1_1hash_3_01linalg_1_1vec_3_01_t_00_011_01_4_01_4.html", null ],
+    [ "hash< linalg::vec< T, 2 > >", "structstd_1_1hash_3_01linalg_1_1vec_3_01_t_00_012_01_4_01_4.html", null ],
+    [ "hash< linalg::vec< T, 3 > >", "structstd_1_1hash_3_01linalg_1_1vec_3_01_t_00_013_01_4_01_4.html", null ],
+    [ "hash< linalg::vec< T, 4 > >", "structstd_1_1hash_3_01linalg_1_1vec_3_01_t_00_014_01_4_01_4.html", null ],
+    [ "hash< linalg::mat< T, M, 1 > >", "structstd_1_1hash_3_01linalg_1_1mat_3_01_t_00_01_m_00_011_01_4_01_4.html", null ],
+    [ "hash< linalg::mat< T, M, 2 > >", "structstd_1_1hash_3_01linalg_1_1mat_3_01_t_00_01_m_00_012_01_4_01_4.html", null ],
+    [ "hash< linalg::mat< T, M, 3 > >", "structstd_1_1hash_3_01linalg_1_1mat_3_01_t_00_01_m_00_013_01_4_01_4.html", null ],
+    [ "hash< linalg::mat< T, M, 4 > >", "structstd_1_1hash_3_01linalg_1_1mat_3_01_t_00_01_m_00_014_01_4_01_4.html", null ]
+];
---
+++ manifold-root.d.ts
@@ -0,0 +1,46 @@
+// Copyright 2022-2025 The Manifold Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * The core WASM bindings with no frills.
+ *
+ * @group manifold
+ * @packageDocumentation
+ * @primaryExport
+ * @see {@link "Using Manifold" | Using Manifold}
+ * @see {@link "Manifold Examples" | Manifold Examples}
+ */
+
+export type * from './manifold-global-types';
+export type {MeshOptions, triangulate, setMinCircularAngle, setMinCircularEdgeLength, setCircularSegments, getCircularSegments, resetToCircularDefaults} from './manifold-encapsulated-types';
+export {CrossSection, Manifold, Mesh} from './manifold-encapsulated-types';
+
+import type {triangulate, setMinCircularAngle, setMinCircularEdgeLength, setCircularSegments, getCircularSegments, resetToCircularDefaults} from './manifold-encapsulated-types';
+import {CrossSection, Manifold, Mesh} from './manifold-encapsulated-types';
+
+export interface ManifoldToplevel {
+  CrossSection: typeof CrossSection;
+  Manifold: typeof Manifold;
+  Mesh: typeof Mesh;
+  triangulate: typeof triangulate;
+  setMinCircularAngle: typeof setMinCircularAngle;
+  setMinCircularEdgeLength: typeof setMinCircularEdgeLength;
+  setCircularSegments: typeof setCircularSegments;
+  getCircularSegments: typeof getCircularSegments;
+  resetToCircularDefaults: typeof resetToCircularDefaults;
+  setup: () => void;
+}
+
+export default function Module(config?: {locateFile: () => string}):
+    Promise<ManifoldToplevel>;
