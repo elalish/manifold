08d1562fe6fa29d7c8597ebd15ece5171988bae7
elalish <elalish@users.noreply.github.com>
2026-02-13 09:03:34 +0000

    deploy: 61c4af291c81588d71807eaca0a74a025221d8cb

---
+++ docs/html/search/all_e.js
@@ -0,0 +1,16 @@
+var searchData=
+[
+  ['negative_0',['Negative',['../classmanifold_1_1_cross_section.html#a9a534b0377c9ca41983d53b0dae0d5a4affb9356ff2b7da85c75c92fa7ea03b8b',1,'manifold::CrossSection']]],
+  ['nlerp_1',['nlerp',['../group__vec__algebra.html#ga80d7b49f19a1d93752a5479cd07a428a',1,'linalg']]],
+  ['nonzero_2',['NonZero',['../classmanifold_1_1_cross_section.html#a9a534b0377c9ca41983d53b0dae0d5a4a3d0365a27a39c0af183445fc4d932a8a',1,'manifold::CrossSection']]],
+  ['normalidx_3',['normalIdx',['../group___mesh_i_o.html#a8bf2a091ffda73fc32c5bf237e00870a',1,'manifold::Material']]],
+  ['normalize_4',['normalize',['../group__vec__algebra.html#ga831cd65b7277c44a791a507e77ea2f0a',1,'linalg']]],
+  ['numcontour_5',['NumContour',['../classmanifold_1_1_cross_section.html#a87c531d99cd313aeb513cd8cfee1641c',1,'manifold::CrossSection']]],
+  ['numdegeneratetris_6',['NumDegenerateTris',['../classmanifold_1_1_manifold.html#af46a22b8e84924f7159ae2a2db6cc956',1,'manifold::Manifold']]],
+  ['numedge_7',['NumEdge',['../classmanifold_1_1_manifold.html#a6358437d05a317c6bd0860f37ea20abf',1,'manifold::Manifold']]],
+  ['numprop_8',['NumProp',['../classmanifold_1_1_manifold.html#a2851aefadd5cbee46c418ec6564e272f',1,'manifold::Manifold']]],
+  ['numprop_9',['numProp',['../structmanifold_1_1_mesh_g_l_p.html#aed2b741b3f096c286368a79060823c2d',1,'manifold::MeshGLP']]],
+  ['numpropvert_10',['NumPropVert',['../classmanifold_1_1_manifold.html#a35df8a9a52cf7dbd13265446d5993db9',1,'manifold::Manifold']]],
+  ['numtri_11',['NumTri',['../structmanifold_1_1_mesh_g_l_p.html#a8458191a8d6a8f4faeca38353a034826',1,'manifold::MeshGLP::NumTri()'],['../classmanifold_1_1_manifold.html#aaaa84bb817bb4f9b58ef22bdc62af686',1,'manifold::Manifold::NumTri()']]],
+  ['numvert_12',['NumVert',['../structmanifold_1_1_mesh_g_l_p.html#abe8f3e86d9e52e570b74cba17026579c',1,'manifold::MeshGLP::NumVert()'],['../classmanifold_1_1_manifold.html#ac8fdbd9a43d0168f1d6e6767aa89bb8d',1,'manifold::Manifold::NumVert()'],['../classmanifold_1_1_cross_section.html#ac8fdbd9a43d0168f1d6e6767aa89bb8d',1,'manifold::CrossSection::NumVert()']]]
+];
---
+++ docs/html/search/classes_c.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['rect_0',['Rect',['../structmanifold_1_1_rect.html',1,'manifold']]]
+];
