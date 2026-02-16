55475ce753118872a0ccb9d6c093f352b65c8ad2
Tony Thompson <tony@thompson.name>
2026-02-15 21:47:46 -0500

    It has been [0] days since the last linter error.

--- bindings/wasm/test/examples/godel-escher-bach.ts
+++ bindings/wasm/test/examples/godel-escher-bach.ts
@@ -7,35 +7,37 @@
 // manifold objects. When exporting to glTF, CrossSections will be visible, but
 // will not have `EXT_mesh_manifold` applied.
 
-import {Font, getGlyphPath, pathToSVG} from 'text-shaper';
+import {CrossSection, CrossSectionGLTFNode, GLTFNode, Manifold, Polygons, Vec3} from 'manifold-3d/manifoldCAD';
 import {pointsOnPath} from 'points-on-path';
-import {CrossSection, Manifold, CrossSectionGLTFNode, GLTFNode, Polygons, Vec3} from 'manifold-3d/manifoldCAD';
+import {Font, getGlyphPath, pathToSVG} from 'text-shaper';
 
-const fonturl = 'https://github.com/theleagueof/orbitron/raw/refs/heads/master/webfonts/orbitron-black-webfont.ttf';
+const fonturl =
+    'https://github.com/theleagueof/orbitron/raw/refs/heads/master/webfonts/orbitron-black-webfont.ttf';
 
 // Return a function that generates a CrossSection for a given letter.
 // Getting the function is asynchronous as it depends on a fetch, but
 // the function itself is synchronous.
-const characterGenerator = async (fonturl:string, height=100) => {
+const characterGenerator =
+    async (fonturl: string, height = 100) => {
   const font = await Font.fromURL(fonturl);
 
-  // Helper function to center and scale a CrossSection
-  // This particular demo looks best if characters are square, so ignore aspect ratio.
-  const resize = (cs:CrossSection) => {
+  // Helper function to center and scale a CrossSection.
+  // This particular demo looks best if characters are square, so ignore aspect
+  // ratio.
+  const resize = (cs: CrossSection) => {
     const {min, max} = cs.bounds();
-    return cs
-        .translate([-(min[0]+max[0])/2,-(min[1]+max[1])/2])
-        .scale([height/(max[0]-min[0]),height/(max[1]-min[1])]);
+    return cs.translate([-(min[0] + max[0]) / 2, -(min[1] + max[1]) / 2])
+        .scale([height / (max[0] - min[0]), height / (max[1] - min[1])]);
   };
 
-  return (char:string): CrossSection => {
+  return (char: string): CrossSection => {
     const glyphpath = getGlyphPath(font, font.glyphIdForChar(char));
     const svgpath = pathToSVG(glyphpath, {flipY: true, scale: 1});
     const cs = new CrossSection(pointsOnPath(svgpath) as Polygons);
-    return resize(cs).mirror([0,1]);
+    return resize(cs).mirror([0, 1]);
   }
 }
- 
+
 export default async () => {
   const height = 100;
   const offset = height / 2;
@@ -82,13 +84,13 @@ export default async () => {
   // Time for some Constructive Solid Geometry.
   // Extrude each CrossSection and apply the same rotation and translation.
   // Then compute the intersection.
-  const extrude = (node:CrossSectionGLTFNode) => {
+  const extrude = (node: CrossSectionGLTFNode) => {
     let extrusion = node.crossSection!.extrude(4 * height);
     extrusion = extrusion.rotate(node.rotation as Vec3);
     extrusion = extrusion.translate(node.translation as Vec3);
     return extrusion;
-  }
-  const intersect = ((acc:Manifold, cur:Manifold) => acc.intersect(cur))
+  };
+  const intersect = ((acc: Manifold, cur: Manifold) => acc.intersect(cur));
   const intersection = csNodes.map(extrude).reduce(intersect);
 
   // Put the result into a node so it can be oriented
