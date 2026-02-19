83f5e03f7dc443ecd8ced3b4913e06c47946d8b4
elalish <elalish@users.noreply.github.com>
2026-02-19 05:30:27 +0000

    deploy: 6c9da566cc65d5d32102ce7d9042d2bbb76847fd

---
+++ docs/html/group__quaternions.js
@@ -0,0 +1,10 @@
+var group__quaternions =
+[
+    [ "qconj", "group__quaternions.html#gac06aafd391bda80a368f27b83e02989b", null ],
+    [ "qinv", "group__quaternions.html#ga104dec8d037ad994f0e22a75624e384f", null ],
+    [ "qexp", "group__quaternions.html#ga21b491be6dd18f8d271bac2c2554a68a", null ],
+    [ "qlog", "group__quaternions.html#ga67c160bbfb91973a753b3896127deddd", null ],
+    [ "qpow", "group__quaternions.html#ga47ad14d41bf6f03fb109e0372e5430d5", null ],
+    [ "qmul", "group__quaternions.html#ga73c877e6b9c605c23a3a4a86828f3111", null ],
+    [ "qmul", "group__quaternions.html#ga6076df08fc6df3112f6b432d7860ec12", null ]
+];
---
+++ docs/html/search/functions_a.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['nlerp_0',['nlerp',['../group__vec__algebra.html#ga80d7b49f19a1d93752a5479cd07a428a',1,'linalg']]],
+  ['normalize_1',['normalize',['../group__vec__algebra.html#ga831cd65b7277c44a791a507e77ea2f0a',1,'linalg']]],
+  ['numcontour_2',['NumContour',['../classmanifold_1_1_cross_section.html#a87c531d99cd313aeb513cd8cfee1641c',1,'manifold::CrossSection']]],
+  ['numdegeneratetris_3',['NumDegenerateTris',['../classmanifold_1_1_manifold.html#af46a22b8e84924f7159ae2a2db6cc956',1,'manifold::Manifold']]],
+  ['numedge_4',['NumEdge',['../classmanifold_1_1_manifold.html#a6358437d05a317c6bd0860f37ea20abf',1,'manifold::Manifold']]],
+  ['numprop_5',['NumProp',['../classmanifold_1_1_manifold.html#a2851aefadd5cbee46c418ec6564e272f',1,'manifold::Manifold']]],
+  ['numpropvert_6',['NumPropVert',['../classmanifold_1_1_manifold.html#a35df8a9a52cf7dbd13265446d5993db9',1,'manifold::Manifold']]],
+  ['numtri_7',['NumTri',['../structmanifold_1_1_mesh_g_l_p.html#a8458191a8d6a8f4faeca38353a034826',1,'manifold::MeshGLP::NumTri()'],['../classmanifold_1_1_manifold.html#aaaa84bb817bb4f9b58ef22bdc62af686',1,'manifold::Manifold::NumTri()']]],
+  ['numvert_8',['NumVert',['../structmanifold_1_1_mesh_g_l_p.html#abe8f3e86d9e52e570b74cba17026579c',1,'manifold::MeshGLP::NumVert()'],['../classmanifold_1_1_manifold.html#ac8fdbd9a43d0168f1d6e6767aa89bb8d',1,'manifold::Manifold::NumVert()'],['../classmanifold_1_1_cross_section.html#ac8fdbd9a43d0168f1d6e6767aa89bb8d',1,'manifold::CrossSection::NumVert()']]]
+];
