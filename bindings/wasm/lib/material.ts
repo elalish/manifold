elalish/manifold

commit 9e1fa6d54476501e8c14a23b13b3d1fe7bf8dbe3

Make CrossSections visible. (#1507)
* Initial cross section support.

* CSG display example

* It has been [0] days since the last linter error.

* Tests and creature comforts.

* Format

* Fixed example.

* BaseGLTFNode subclasses refactored for consistency.

* Use gltf-io when writing CrossSectionGLTFNodes.

* Revert GLTFNode.addChild() and listChildren()

* Refactor CrossSection and Manifold export for consistency

* A little more cleanup.

* Use a font to generate characters

* It has been [0] days since the last linter error.

bindings/wasm/lib/material.ts
@@ -27,7 +27,7 @@ import {copyToDocument} from '@gltf-transform/functions';
 import type {Manifold} from '../manifold.d.ts';
 
 import type {GLTFMaterial} from './gltf-node.ts';
-import {GLTFNode} from './gltf-node.ts';
+import {BaseGLTFNode, CrossSectionGLTFNode, GLTFNode} from './gltf-node.ts';
 import {getDocumentByID} from './import-model.ts';
 
 const defaultMaterial = {
@@ -36,6 +36,7 @@ const defaultMaterial = {
   baseColorFactor: [1, 1, 0] as [number, number, number],
   alpha: 1,
   unlit: false,
+  doubleSided: false
 };
 
 const id2material = new Map<number, GLTFMaterial>();
@@ -75,16 +76,18 @@ export const setMaterialByID = (id: number, material: GLTFMaterial) => {
 };
 
 /**
+ * Recurse up the scene graph to find a material.
  * @internal
  */
-export function getBackupMaterial(node?: GLTFNode): GLTFMaterial {
-  if (node == null) {
-    return {};
+export function getBackupMaterial(node?: BaseGLTFNode): GLTFMaterial {
+  if (node != null &&
+      (node instanceof GLTFNode || node instanceof CrossSectionGLTFNode)) {
+    if (node.material == null) {
+      return getBackupMaterial((node.parent as GLTFNode));
+    }
+    return node.material;
   }
-  if (node.material == null) {
-    node.material = getBackupMaterial((node.parent as GLTFNode));
-  }
-  return node.material;
+  return {}
 }
 
 function copyImportedMaterial(
@@ -104,7 +107,7 @@ function copyImportedMaterial(
 function makeDefaultMaterial(
     doc: GLTFTransform.Document,
     matIn: GLTFMaterial = {}): GLTFTransform.Material {
-  const {roughness, metallic, baseColorFactor, alpha, unlit} = {
+  const {roughness, metallic, baseColorFactor, alpha, unlit, doubleSided} = {
     ...defaultMaterial,
     ...matIn
   };
@@ -123,7 +126,8 @@ function makeDefaultMaterial(
 
   return material.setRoughnessFactor(roughness)
       .setMetallicFactor(metallic)
-      .setBaseColorFactor([...baseColorFactor, alpha]);
+      .setBaseColorFactor([...baseColorFactor, alpha])
+      .setDoubleSided(!!doubleSided);
 }
 
 /**

bindings/wasm/.gitignore
@@ -6,6 +6,10 @@ lib/*.js
 lib/*.js.map
 lib/*.d.ts
 lib/*.d.ts.map
+test/*.js
+test/*.js.map
+test/*.d.ts
+test/*.d.ts.map
 dist/
 coverage/
 test/results

bindings/wasm/examples/editor-examples.js
@@ -1,5 +1,6 @@
 import auger from '../test/examples/auger.mjs?raw';
 import gearBearing from '../test/examples/gear-bearing.ts?raw';
+import godelEscherBach from '../test/examples/godel-escher-bach.ts?raw';
 import gyroidModule from '../test/examples/gyroid-module.ts?raw';
 import heart from '../test/examples/heart.mjs?raw';
 import importManifold from '../test/examples/import-manifold.ts?raw';
@@ -28,6 +29,7 @@ examples.set('Gear Bearing', gearBearing);
 examples.set('involute-gear-library', involuteGearLibrary);
 examples.set('Voronoi', voronoi);
 examples.set('Import Manifold', importManifold);
+examples.set('Gödel, Escher, Bach', godelEscherBach)
 
 if (typeof self !== 'undefined') {
   self.examples = examples;

bindings/wasm/lib/animation.ts
@@ -284,6 +284,7 @@ export function setMorphEnd(manifold: Manifold, func: (v: Vec3) => void):
  * @internal
  */
 export function addAnimationToDoc(doc: Document) {
+  const buffer = doc.getRoot().listBuffers()[0];
   animation = doc.createAnimation('');
   hasAnimation = false;
   const nFrames = Math.round(animationDuration * animationFPS) + 1;
@@ -296,11 +297,11 @@ export function addAnimationToDoc(doc: Document) {
         animationMode !== 'ping-pong' ? x : (1 - Math.cos(x * 2 * Math.PI)) / 2;
   }
   timesAccessor = doc.createAccessor('animation times')
-                      .setBuffer(doc.createBuffer())
+                      .setBuffer(buffer)
                       .setArray(times)
                       .setType(Accessor.Type.SCALAR);
   weightsAccessor = doc.createAccessor('animation weights')
-                        .setBuffer(doc.getRoot().listBuffers()[0])
+                        .setBuffer(buffer)
                         .setArray(weights)
                         .setType(Accessor.Type.SCALAR);
   weightsSampler = doc.createAnimationSampler()

bindings/wasm/lib/bundler.ts
@@ -112,6 +112,7 @@ export const esbuildManifoldPlugin = (options: BundlerOptions = {}):
       'show', 'only', 'setMaterial',
       // GLTFNode and utilities.
       'GLTFMaterial', 'GLTFNode', 'getGLTFNodes', 'VisualizationGLTFNode',
+      'CrossSectionGLTFNode',
       // Import
       'importModel', 'importManifold',
       // Getters for global properties

bindings/wasm/lib/gltf-io.ts
@@ -245,36 +245,46 @@ export function readMesh(
  *     the manifoldMesh vertProperties, thus the first attribute must always be
  *     'POSITION'. Any properties that should not be output for a given
  *     primitive must use the 'SKIP_*' attributes.
+ * @param EXT_mesh_manifold If false, emit a plain glTF mesh.  In this case,
+ *     the mesh is not required to be fully manifold when written.  Use this
+ *     to write explicitly non-manifold meshes, e.g.: CrossSections.
  * @returns The glTF Mesh to add to the Document.
  */
 export function writeMesh(
     doc: GLTFTransform.Document, manifoldMesh: ManifoldMesh,
-    id2properties: Map<number, Properties>): GLTFTransform.Mesh {
-  if (doc.getRoot().listBuffers().length === 0) {
-    doc.createBuffer();
-  }
-  const buffer = doc.getRoot().listBuffers()[0];
-  const manifoldExtension = doc.createExtension(EXTManifold);
-
+    id2properties: Map<number, Properties>,
+    EXT_mesh_manifold: boolean = true): GLTFTransform.Mesh {
   const mesh = doc.createMesh();
-  const runIndex = Array<number>();
+
+  writePrimitiveAttributes(doc, mesh, manifoldMesh, id2properties);
+  if (EXT_mesh_manifold) {
+    writeExtMeshManifoldIndices(doc, mesh, manifoldMesh);
+  } else {
+    writePlainIndices(doc, mesh, manifoldMesh);
+  }
+
+  return mesh;
+}
+
+/**
+ * Create the necessary primitives and their attributes needed to represent a
+ * ManifoldMesh object as a glTF mesh, and add those to an existing glTF
+ * transform mesh node.
+ *
+ * This does not create or populate indices.  After this call these primitives
+ * will exist and have positions, but will not have visible geometry.
+ */
+function writePrimitiveAttributes(
+    doc: GLTFTransform.Document, mesh: GLTFTransform.Mesh,
+    manifoldMesh: ManifoldMesh, id2properties: Map<number, Properties>) {
   const attributeUnion = Array<Attribute>();
   const primitive2attributes = new Map<GLTFTransform.Primitive, Attribute[]>();
-  const numRun = manifoldMesh.runIndex.length - 1;
-  let lastID = -1;
-  for (let run = 0; run < numRun; ++run) {
-    const id = manifoldMesh.runOriginalID[run];
-    if (id == lastID) {
-      continue;
-    }
-    lastID = id;
-    runIndex.push(manifoldMesh.runIndex[run]);
 
-    const indices = doc.createAccessor('primitive indices of ID ' + id)
-                        .setBuffer(buffer)
-                        .setType(GLTFTransform.Accessor.Type.SCALAR)
-                        .setArray(new Uint32Array(1));
-    const primitive = doc.createPrimitive().setIndices(indices);
+  // For each run, create a primitive, set material and collate attributes.
+  const buffer = doc.getRoot().listBuffers()[0];
+  for (let run = 0; run < (manifoldMesh.runIndex.length - 1); run++) {
+    const id = manifoldMesh.runOriginalID[run];
+    const primitive = doc.createPrimitive();
 
     const properties = id2properties.get(id);
     if (properties) {
@@ -307,8 +317,9 @@ export function writeMesh(
 
     mesh.addPrimitive(primitive);
   }
-  runIndex.push(manifoldMesh.runIndex[numRun]);
 
+  // For each primitive, create accessors for each attribute and populate those
+  // attributes.
   const numVert = manifoldMesh.numVert;
   const numProp = manifoldMesh.numProp;
   let offset = 0;
@@ -350,9 +361,27 @@ export function writeMesh(
     }
     offset += n;
   });
+}
 
+function writeExtMeshManifoldIndices(
+    doc: GLTFTransform.Document, mesh: GLTFTransform.Mesh,
+    manifoldMesh: ManifoldMesh) {
+  const manifoldExtension = doc.createExtension(EXTManifold);
   const manifoldPrimitive = manifoldExtension.createManifoldPrimitive();
   mesh.setExtension('EXT_mesh_manifold', manifoldPrimitive);
+
+  const buffer = doc.getRoot().listBuffers()[0];
+  const {runIndex} = manifoldMesh;
+
+  mesh.listPrimitives().forEach((primitive, n) => {
+    // These indices will be populated by `manifold-gtlf` when the
+    // document is written out by glTF transform.
+    const indices = doc.createAccessor('primitive indices of ID ' + runIndex[n])
+                        .setBuffer(buffer)
+                        .setType(GLTFTransform.Accessor.Type.SCALAR)
+                        .setArray(new Uint32Array(1));
+    primitive.setIndices(indices);
+  });
 
   const indices =
       doc.createAccessor('manifold indices')
@@ -389,8 +418,22 @@ export function writeMesh(
                                .setArray(new Uint32Array(val));
     manifoldPrimitive.setMerge(indicesAccessor, valuesAccessor);
   }
+}
 
-  return mesh;
+function writePlainIndices(
+    doc: GLTFTransform.Document, mesh: GLTFTransform.Mesh,
+    manifoldMesh: ManifoldMesh) {
+  const buffer = doc.getRoot().listBuffers()[0];
+  const {runIndex} = manifoldMesh;
+
+  mesh.listPrimitives().forEach((primitive, n) => {
+    const indices = doc.createAccessor('primitive indices of ID ' + runIndex[n])
+                        .setBuffer(buffer)
+                        .setType(GLTFTransform.Accessor.Type.SCALAR)
+                        .setArray(manifoldMesh.triVerts.slice(
+                            runIndex[n], runIndex[n + 1]));
+    primitive.setIndices(indices);
+  });
 }
 
 /**

bindings/wasm/lib/gltf-node.test.ts
@@ -1,9 +1,24 @@
+// Copyright 2026 The Manifold Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import {afterEach, beforeAll, describe, expect, suite, test} from 'vitest';
 
 import type {Vec3} from '../manifold.d.ts';
 import {equalsVec3Array, meshToVec3Array} from '../test/util.ts';
 
 import {toArrayBuffer} from './export-model.ts';
+import {BaseGLTFNode, CrossSectionGLTFNode, GLTFNode, GLTFNodeTracked, VisualizationGLTFNode} from './gltf-node.ts';
 import * as importer from './import-model.ts';
 import * as wasm from './wasm.ts';
 import * as worker from './worker.ts';
@@ -54,5 +69,157 @@ suite('Ensure manifold and glTF rotations match', () => {
         });
       });
     });
+  });
+});
+
+suite('GLTFNode', () => {
+  test('clone()', () => {
+    const parent = new GLTFNode();
+    const node = new GLTFNode(parent);
+    const clone = node.clone();
+
+    expect(clone).toBeInstanceOf(BaseGLTFNode);
+    expect(clone).toBeInstanceOf(GLTFNode);
+    expect(clone.parent).toEqual(parent);
+  });
+
+  test('clone(newParent)', () => {
+    const parent = new GLTFNode();
+    const node = new GLTFNode(parent);
+    const cloneParent = new GLTFNode();
+    const clone = node.clone(cloneParent);
+
+    expect(clone).toBeInstanceOf(BaseGLTFNode);
+    expect(clone).toBeInstanceOf(GLTFNode);
+    expect(clone.parent).toEqual(cloneParent);
+  });
+});
+
+suite('GLTFNodeTracked', () => {
+  test('clone()', () => {
+    const parent = new GLTFNode();
+    const node = new GLTFNodeTracked(parent);
+    const clone = node.clone();
+
+    expect(clone).toBeInstanceOf(BaseGLTFNode);
+    expect(clone).toBeInstanceOf(GLTFNode);
+    expect(clone).toBeInstanceOf(GLTFNodeTracked);
+    expect(clone.parent).toEqual(parent);
+  });
+
+  test('clone(newParent)', () => {
+    const parent = new GLTFNode();
+    const node = new GLTFNodeTracked(parent);
+    const cloneParent = new GLTFNode();
+    const clone = node.clone(cloneParent);
+
+    expect(clone).toBeInstanceOf(BaseGLTFNode);
+    expect(clone).toBeInstanceOf(GLTFNode);
+    expect(clone).toBeInstanceOf(GLTFNodeTracked);
+    expect(clone.parent).toEqual(cloneParent);
+  });
+});
+
+suite('VisualizationGLTFNode', () => {
+  test('clone()', async () => {
+    const url = new URL('../test/fixtures/models/box.glb', import.meta.url);
+    const doc = await importer.readModel(url);
+    const parent = new GLTFNode();
+    const node = new VisualizationGLTFNode(parent);
+    node.document = doc;
+    const clone = node.clone();
+
+    expect(clone).toBeInstanceOf(BaseGLTFNode);
+    expect(clone).toBeInstanceOf(VisualizationGLTFNode);
+    expect(clone.document).toEqual(doc);
+    expect(clone.parent).toEqual(parent);
+  });
+
+  test('clone(newParent)', async () => {
+    const url = new URL('../test/fixtures/models/box.glb', import.meta.url);
+    const doc = await importer.readModel(url);
+    const parent = new GLTFNode();
+    const node = new VisualizationGLTFNode(parent);
+    node.document = doc;
+    const cloneParent = new GLTFNode();
+    const clone = node.clone(cloneParent);
+
+    expect(clone).toBeInstanceOf(BaseGLTFNode);
+    expect(clone).toBeInstanceOf(VisualizationGLTFNode);
+    expect(clone.document).toEqual(doc);
+    expect(clone.parent).toEqual(cloneParent);
+  });
+});
+
+suite('CrossSectionGTLFNode', () => {
+  test('clone()', async () => {
+    const parent = new GLTFNode();
+    const node = new CrossSectionGLTFNode(parent)
+    const clone = node.clone();
+
+    expect(clone).toBeInstanceOf(BaseGLTFNode);
+    expect(clone).toBeInstanceOf(CrossSectionGLTFNode);
+    expect(clone.parent).toEqual(parent);
+  });
+
+  test('clone(newParent)', async () => {
+    const parent = new GLTFNode();
+    const node = new CrossSectionGLTFNode(parent)
+    const cloneParent = new GLTFNode();
+    const clone = node.clone(cloneParent);
+
+    expect(clone).toBeInstanceOf(BaseGLTFNode);
+    expect(clone).toBeInstanceOf(CrossSectionGLTFNode);
+    expect(clone.parent).toEqual(cloneParent);
+  });
+});
+
+suite('anyToGLTFNodeList()', () => {
+  test('Rejects generic objects', async () => {
+    const script = `export default {};`;
+
+    const fn = async () => await worker.evaluate(script);
+    await expect(fn()).rejects.toThrow();
+  });
+
+  test('Rejects empty arrays', async () => {
+    const script = `export default [];`;
+
+    const fn = async () => await worker.evaluate(script);
+    await expect(fn()).rejects.toThrow();
+  });
+
+  test('Accepts CrossSection objects', async () => {
+    const script = `import {CrossSection} from 'manifold-3d/manifoldCAD';\n` +
+        `export default CrossSection.circle(1.0);`;
+
+    const fn = async () => await worker.evaluate(script);
+    await expect(fn()).resolves.toBeDefined();
+  });
+
+  test('Accepts Manifold objects', async () => {
+    const script = `import {Manifold} from 'manifold-3d/manifoldCAD';\n` +
+        `export default Manifold.cube(1.0);`;
+
+    const fn = async () => await worker.evaluate(script);
+    await expect(fn()).resolves.toBeDefined();
+  });
+
+  test('Accepts empty Manifold objects', async () => {
+    const script = `import {Manifold} from 'manifold-3d/manifoldCAD';\n` +
+        `export default Manifold.hull([]);`;
+
+    const fn = async () => await worker.evaluate(script);
+    await expect(fn()).resolves.toBeDefined();
+  });
+
+  test('Throws when .crossSection is empty', async () => {
+    const script =
+        `import {CrossSectionGLTFNode} from 'manifold-3d/manifoldCAD';\n` +
+        `const node = new CrossSectionGLTFNode()` +
+        `export default node;`;
+
+    const fn = async () => await worker.evaluate(script);
+    await expect(fn()).rejects.toThrow();
   });
 });

bindings/wasm/lib/gltf-node.ts
@@ -37,7 +37,9 @@
 
 import type * as GLTFTransform from '@gltf-transform/core';
 
-import type {Manifold, Vec3} from '../manifold.d.ts';
+import type {CrossSection, Manifold, Vec3} from '../manifold.d.ts';
+
+import {getManifoldModuleSync} from './wasm.ts';
 
 const nodes = new Array<BaseGLTFNode>();
 
@@ -148,6 +150,14 @@ export interface GLTFMaterial {
    * @internal
    */
   sourceRunID?: number;
+
+  /**
+   * Treat this material as double sided.
+   * This is implied for translucent materials (`alpha < 1.0`) and
+   * CrossSections.
+   * @internal
+   */
+  doubleSided?: boolean;
 }
 
 /**
@@ -157,7 +167,7 @@ export interface GLTFMaterial {
  */
 export abstract class BaseGLTFNode {
   /** @internal */
-  _parent?: BaseGLTFNode;
+  private _parent?: BaseGLTFNode;
   name?: string;
 
   // Internally, gltf-transform stores transformations as separate translation,
@@ -184,6 +194,14 @@ export abstract class BaseGLTFNode {
   get parent() {
     return this._parent;
   }
+
+  /**
+   * Does this node have any geometry that needs to be converted on export?
+   * @internal
+   */
+  isEmpty(): boolean {
+    return true;
+  }
 }
 
 /**
@@ -198,6 +216,10 @@ export class GLTFNode extends BaseGLTFNode {
     const copy = new GLTFNode(newParent ?? this.parent);
     Object.assign(copy, this);
     return copy;
+  }
+
+  isEmpty() {
+    return this.manifold?.isEmpty() ?? true;
   }
 }
 
@@ -238,23 +260,72 @@ export class VisualizationGLTFNode extends BaseGLTFNode {
   /** @internal */
   node?: GLTFTransform.Node;
   /** @internal */
-  document: GLTFTransform.Document;
+  document?: GLTFTransform.Document;
   /** @internal */
   uri?: string;
 
-  constructor(
-      document: GLTFTransform.Document, node?: GLTFTransform.Node,
-      parent?: BaseGLTFNode) {
+  constructor(parent?: BaseGLTFNode) {
     super(parent);
-    this.document = document;
-    this.node = node;
   }
 
   clone(newParent?: BaseGLTFNode) {
-    const copy = new VisualizationGLTFNode(
-        this.document, this.node, newParent ?? this.parent);
+    const copy = new VisualizationGLTFNode(newParent ?? this.parent);
     Object.assign(copy, this);
     return copy;
+  }
+
+  isEmpty() {
+    return !this.document;
+  }
+}
+
+/**
+ * Display a CrossSection in 3D space.
+ *
+ * A CrossSection object is two dimensional.  Attaching it as a node
+ * allows it to be included in the final exported file, complete with
+ * transformations.
+ *
+ * > [!NOTICE]
+ * >
+ * > CrossSections are not -- and can never be -- manifold.  That means
+ * > some exporters (like `.3mf`) will just skip over them entirely.
+ *
+ * @group Scene Graph
+ */
+export class CrossSectionGLTFNode extends BaseGLTFNode {
+  crossSection?: CrossSection;
+  material?: GLTFMaterial;
+
+  private _runid?: number;
+
+  constructor(parent?: BaseGLTFNode) {
+    super(parent);
+  }
+
+  clone(newParent?: BaseGLTFNode) {
+    const copy = new CrossSectionGLTFNode(newParent ?? this.parent);
+    Object.assign(copy, this);
+    return copy;
+  }
+
+  isEmpty() {
+    return this.crossSection?.isEmpty() ?? true;
+  }
+
+  /**
+   * Get the runID for this node.
+   * If there is no runID set, lazily assign one.
+   *
+   * We don't need these for regular operations, but they do help when
+   * converting to meshes for export.
+   * @internal
+   */
+  get runID(): number {
+    if (this._runid === undefined) {
+      this._runid = getManifoldModuleSync()!.Manifold.reserveIDs(1);
+    }
+    return this._runid;
   }
 }
 
@@ -298,20 +369,22 @@ export const cleanup = () => {
  * @returns An array of GLTFNodes.
  */
 export async function anyToGLTFNodeList(
-    any: Manifold|BaseGLTFNode|
-    Array<Manifold|BaseGLTFNode>): Promise<Array<BaseGLTFNode>> {
+    any: Manifold|BaseGLTFNode|CrossSection|
+    Array<Manifold|BaseGLTFNode|CrossSection>): Promise<Array<BaseGLTFNode>> {
   if (Array.isArray(any)) {
     return await any.map(anyToGLTFNodeList)
         .reduce(
             async (acc, cur) => ([...(await acc), ...(await cur)]),
             new Promise(resolve => resolve([])));
   } else if (any instanceof BaseGLTFNode) {
-    const node = any as BaseGLTFNode;
-    if (!node.parent) return [node];
-    return [await anyToGLTFNodeList(node.parent), node].flat();
+    return [any];
   } else if (any.constructor.name === 'Manifold') {
     const node = new GLTFNode();
     node.manifold = any as Manifold;
+    return [node];
+  } else if (any.constructor.name === 'CrossSection') {
+    const node = new CrossSectionGLTFNode();
+    node.crossSection = any as CrossSection;
     return [node];
   }
 

commit 6783f899c55d0a776db4c44b6dd2a8ba39d35e40

Material transparency implies doubleSided.
Bug introduced in #1507.  The flag was intended to make doubleSided
materials available for CrossSections, but failed to take alpha into
account.

bindings/wasm/lib/material.ts
@@ -120,14 +120,13 @@ function makeDefaultMaterial(
   }
 
   if (alpha < 1) {
-    material.setAlphaMode(GLTFTransform.Material.AlphaMode.BLEND)
-        .setDoubleSided(true);
+    material.setAlphaMode(GLTFTransform.Material.AlphaMode.BLEND);
   }
 
   return material.setRoughnessFactor(roughness)
       .setMetallicFactor(metallic)
       .setBaseColorFactor([...baseColorFactor, alpha])
-      .setDoubleSided(!!doubleSided);
+      .setDoubleSided(!!doubleSided || alpha < 1);
 }
 
 /**