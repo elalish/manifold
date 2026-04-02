869c89f169b6f98802f6211ffb4efa17c2331757
pca006132 <pca006132@users.noreply.github.com>
2026-04-02 11:28:21 +0000

    deploy: 3cebb542cdb5038527af464ff5bba30d41a26fc8

---
+++ docs/html/search/variables_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['cleanuptriangles_0',['cleanupTriangles',['../group___debug.html#a85c1664e476e1da1b3d0973b3451857b',1,'manifold::ExecutionParams']]]
+];
---
+++ docs/html/search/variables_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['halfedge_0',['halfedge',['../group___structs.html#aac5709f1a46385ba2978c8184909a8f9',1,'manifold::Smoothness']]],
+  ['halfedgetangent_1',['halfedgeTangent',['../structmanifold_1_1_mesh_g_l_p.html#a987ae155e6c5981adb174a4898bfdfab',1,'manifold::MeshGLP']]]
+];
