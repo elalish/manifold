aced53bf9a08124f4ea4c2b254e29eacdc7ab4fa
elalish <elalish@users.noreply.github.com>
2026-04-26 07:44:38 +0000

    deploy: 396bc5841d3550dbacbc82f9a44be431de798138

---
+++ assets/environment.js
@@ -0,0 +1,1 @@
+import{d as e}from"./htmlContent.js";const o=e("environmentService");export{o as I};
---
+++ docs/html/search/functions_c.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['progress_0',['Progress',['../classmanifold_1_1_execution_context.html#aa480c2bfebb7c2cf5613eccc969dcd02',1,'manifold::ExecutionContext']]],
+  ['project_1',['Project',['../classmanifold_1_1_manifold.html#a328657b69e9bbaaee148572ca314677c',1,'manifold::Manifold']]]
+];
