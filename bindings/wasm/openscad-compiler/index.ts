b42d4a07e2a68af3cfc2f6f296287655b9096753
swaparup36 <swaparup36@gmail.com>
2026-07-23 11:44:13 +0530

    fix: formatting

--- bindings/wasm/openscad-compiler/index.ts
+++ bindings/wasm/openscad-compiler/index.ts
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-import { Command } from "commander";
-import compileSingleFileCommand from "./commands/compile.js";
-import compileAllCommand from "./commands/compile_all.js";
+import {Command} from 'commander';
+import compileSingleFileCommand from './commands/compile.js';
+import compileAllCommand from './commands/compile_all.js';
 
 const program = new Command();
 
-program.name("openscad-to-manifold")
-    .description("Convert OpenSCAD files to manifold mesh files")
-    .version("1.0.0")
+program.name('openscad-to-manifold')
+    .description('Convert OpenSCAD files to manifold mesh files')
+    .version('1.0.0')
 
 program.addCommand(compileSingleFileCommand);
 program.addCommand(compileAllCommand);
