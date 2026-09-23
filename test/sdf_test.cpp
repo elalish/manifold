59505fc2e0f26f8328e17b9dd7264e6be958f251
Robert Kossessa <github@kosro.de>
2026-09-23 22:17:40 +0200

    Double SDF determinism test edge_length and rename it to SDF.Determinism Test still fails without the fix at the increased edge_length. I also renamed it to match the other Determinism tests.

--- test/sdf_test.cpp
+++ test/sdf_test.cpp
@@ -180,15 +180,15 @@ TEST(SDF, SineSurface) {
   if (options.exportModels) WriteTestOBJ("sinesurface.obj", surface);
 }
 
-TEST(SDF, Deterministic) {
+TEST(SDF, Determinism) {
   const auto gyroid = [](vec3 p) {
     return la::sin(p.x) * la::cos(p.y) + la::sin(p.y) * la::cos(p.z) +
            la::sin(p.z) * la::cos(p.x);
   };
   const Box bounds = {vec3(-10), vec3(10)};
-  const MeshGL first = Manifold::LevelSet(gyroid, bounds, 0.5).GetMeshGL();
+  const MeshGL first = Manifold::LevelSet(gyroid, bounds, 1.0).GetMeshGL();
   for (int i = 0; i < 10; ++i) {
-    const MeshGL mesh = Manifold::LevelSet(gyroid, bounds, 0.5).GetMeshGL();
+    const MeshGL mesh = Manifold::LevelSet(gyroid, bounds, 1.0).GetMeshGL();
     ASSERT_EQ(mesh.vertProperties, first.vertProperties);
     ASSERT_EQ(mesh.triVerts, first.triVerts);
   }
